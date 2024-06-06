46e422c5417263f81d5f2e85d8a14a6b80b72c80
Dor Harpaz <dor@classiq.io>
2024-06-06 11:37:07 +0300

    split notebook parsing to a separate file

--- tests/test_links.py
+++ tests/test_links.py
@@ -1,13 +1,9 @@
-import os
 import re
 from collections.abc import Iterable
-from pathlib import Path
 
 import httpx
 import nbformat
-
-ROOT_DIRECTORY = Path(__file__).parent
-
+from utils_for_tests import iterate_notebooks
 
 # the regex below is taken from this stackoverflow:
 #   https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
@@ -18,14 +14,14 @@ URL_IN_MARKDOWN_REGEX = re.compile(r"(?<=\]\()%s(?=\s*\))" % URL_REGEX)
 
 
 def test_links() -> None:
-    for notebook_path in _get_notebooks():
-        for cell_index, url in _get_links_from_file(notebook_path):
+    for notebook_path in iterate_notebooks():
+        for cell_index, url in iterate_links_from_notebook(notebook_path):
             assert _test_single_url(
                 url
             ), f'Broken link found! in file "{notebook_path}", cell number {cell_index} (counting only markdown cells), broken url: "{url}"'
 
 
-def _get_links_from_file(filename: str) -> Iterable[tuple[int, str]]:
+def iterate_links_from_notebook(filename: str) -> Iterable[tuple[int, str]]:
     with open(filename) as f:
         notebook_data = nbformat.read(f, nbformat.NO_CONVERT)  # type: ignore[no-untyped-call]
 
@@ -46,22 +42,3 @@ def _test_single_url(url: str) -> bool:
         return response.is_success
     except httpx.HTTPError:
         return False
-
-
-def _get_notebooks() -> Iterable[str]:
-    if os.environ.get("SHOULD_TEST_ALL_FILES", "") == "true":
-        notebooks_to_test = _get_all_notebooks()
-    else:
-        if os.environ.get("HAS_ANY_IPYNB_CHANGED", "") == "true":
-            notebooks_to_test = os.environ.get("LIST_OF_IPYNB_CHANGED", "").split()
-        else:
-            notebooks_to_test = []
-
-    return notebooks_to_test
-
-
-def _get_all_notebooks(directory: Path = ROOT_DIRECTORY) -> Iterable[str]:
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".ipynb"):
-                yield os.path.join(root, file)
