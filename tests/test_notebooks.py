8daf9fedefd2107e84be8f4401f53bec5994eaca
Or Samimi Golan <orsa@classiq.io>
2024-08-22 15:43:39 +0300

    a fix to the testing issue

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,3 +1,4 @@
+import os
 from testbook import testbook  # type: ignore[import]
 from utils_for_tests import iterate_notebooks
 
@@ -6,5 +7,6 @@ TIMEOUT: int = 60 * 3  # 3 minutes
 
 def test_notebooks() -> None:
     for notebook_path in iterate_notebooks():
+        os.chdir(os.path.dirname(notebook_path))
         with testbook(notebook_path, execute=True, timeout=TIMEOUT):
             pass  # we simply wish it to run without errors
