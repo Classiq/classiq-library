01f333a44036ad7265a5c3bc5b86f01e08d2ee72
Zach Haitin <zacharya@classiq.io>
2024-09-04 14:37:28 +0300

    feat(actions): run tests against nightly

---
+++ .github/scripts/install_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+# Pre is needed for Dev pre releases
+python -m pip install --extra-index-url https://pypi.org/simple --pre -U -r requirements.txt
+python -m pip install --extra-index-url https://pypi.org/simple -U -r requirements_tests.txt
