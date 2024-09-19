5c7a6f50f8fe107046e149a66c6140a3e8eb0791
Nadav Ben Ami <nadav.ben-ami@NadavBenAmi-MacBook-Pro.local>
2024-09-19 14:11:55 +0300

    Re-factor library test CI and Action

--- .github/scripts/install_deps.sh
+++
@@ -1,5 +0,0 @@
-set -e
-
-# Pre is needed for Dev pre releases
-python -m pip install --extra-index-url https://pypi.org/simple --pre -U -r requirements.txt
-python -m pip install --extra-index-url https://pypi.org/simple -U -r requirements_tests.txt
