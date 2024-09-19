c3d7dcb17a4cf3fa4f1f47d630bc855f5f59f7d9
Nadav Ben Ami <nadav.ben-ami@NadavBenAmi-MacBook-Pro.local>
2024-09-19 14:11:55 +0300

    Re-factor library test CI and Action

--- .github/scripts/get_m2m_token.sh
+++ .github/scripts/get_m2m_token.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 set -e
 
-export SECRET_ARN=$PROD_M2M_SECRET_ARN
+export SECRET_ARN=$M2M_SECRET_ARN
 if [ "$IS_DEV" = "true" ]; then
-    SECRET_ARN=$NIGHTLY_M2M_SECRET_ARN
     aws codeartifact login --tool pip --domain classiq-cadmium --repository Pypi-Classiq-Non-Prod
 fi
 
