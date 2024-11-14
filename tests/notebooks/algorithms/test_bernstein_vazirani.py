dadc5b18f676ce3c62d74066238c0a5ca51ac0f4
Nadav Ben Ami <nadav.ben-ami@NadavBenAmi-MacBook-Pro.local>
2024-11-14 14:39:09 +0200

    Add a test using testbook to test the content of the Bernstein Vazirani notebook

---
+++ tests/notebooks/algorithms/test_bernstein_vazirani.py
@@ -0,0 +1,17 @@
+from demo_tests.testbook_utils import (
+    assert_unchanged_size,
+    assert_valid_model,
+    execute_testbook_with_timeout,
+)
+from testbook.client import TestbookNotebookClient  # type: ignore[import]
+
+
+@execute_testbook_with_timeout("bernstein_vazirani", timeout_seconds=20)
+def test_bernstein_vazirani(tb: TestbookNotebookClient) -> None:
+    # test models
+    assert_valid_model(tb.ref("qmod"))
+    # test quantum programs
+    assert_unchanged_size(tb.ref("qprog"), expected_width=6, expected_depth=5)
+
+    # test notebook content
+    assert int(tb.ref("secret_integer_q")) == tb.ref("SECRET_INT")
