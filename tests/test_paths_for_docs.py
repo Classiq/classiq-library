fd04bf7a2d20652e59cbc580a36095a4e9abd6cd
srulre <srulre@gmail.com>
2024-11-19 21:40:15 +0200

    add tests for paths to copy for docs

---
+++ tests/test_paths_for_docs.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+from pathlib import Path
+
+import yaml
+
+ROOT = Path(subprocess.getoutput("git rev-parse --show-toplevel"))  # noqa: S605
+PATHS_FOR_DOCS = ROOT / ".internal" / "paths_for_docs.yaml"
+
+"""
+This test checks if all paths in the files_to_copy_for_docs.yaml file exist.
+The files are required for building the documentation, and are copied to the
+docs after the Jupyter to Markdown conversion.
+"""
+
+
+def test_paths_to_copy_for_docs():
+    with open(PATHS_FOR_DOCS, "r") as file:
+        paths_to_copy = yaml.safe_load(file)
+
+    missing_paths: list[str] = []
+    for path in paths_to_copy.values():
+        # png files are byproduct of the conversion from Jupyter to Markdown and cannot
+        # be checked for existence in advance
+        if not (ROOT / path).exists() and Path(path).suffix != ".png":
+            missing_paths.append(path)
+
+    assert not missing_paths, f"The following paths do not exist: {missing_paths}"
