21c9dee656bab64e8d2c76115a2c6713793c6c1a
Dor Harpaz <dor@classiq.io>
2024-12-12 11:28:55 +0200

    Add test for unique notebook names

---
+++ tests/internal/test_notebook_unique_name.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+
+ROOT = Path(__file__).parents[2]
+
+
+def test_unique_notebook_name():
+    all_notebooks = ROOT.rglob("*.ipynb")
+    assert _are_all_base_names_unique(all_notebooks)
+
+
+def test_unique_qmod_name():
+    all_qmods = ROOT.rglob("*.qmod")
+    assert _are_all_base_names_unique(all_qmods)
+
+
+def _are_all_base_names_unique(files: Iterable[Path]) -> bool:
+    base_names = [f.name for f in files]
+    return len(base_names) == len(set(base_names))
