5436727a90beda1bba7964fa3ee68fd8a8264bfd
Dor Harpaz <dor@classiq.io>
2024-12-12 11:43:46 +0200

    Update error message

--- tests/internal/test_notebook_unique_name.py
+++ tests/internal/test_notebook_unique_name.py
@@ -7,14 +7,14 @@ ROOT = Path(__file__).parents[2]
 
 def test_unique_notebook_name():
     all_notebooks = ROOT.rglob("*.ipynb")
-    assert _are_all_base_names_unique(all_notebooks)
+    assert not duplicate_base_names(all_notebooks)
 
 
 def test_unique_qmod_name():
     all_qmods = ROOT.rglob("*.qmod")
-    assert _are_all_base_names_unique(all_qmods)
+    assert not duplicate_base_names(all_qmods)
 
 
-def _are_all_base_names_unique(files: Iterable[Path]) -> bool:
+def duplicate_base_names(files: Iterable[Path]) -> bool:
     base_names = [f.name for f in files]
-    return len(base_names) == len(set(base_names))
+    return [name for name, count in Counter(base_names).items() if count > 1]
