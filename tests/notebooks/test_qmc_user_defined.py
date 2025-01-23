e71229e4b469cf74855abcc6711b0d65a0104a3c
Dor Harpaz <dor@classiq.io>
2025-01-23 17:49:58 +0200

    Add tests for several notebooks

---
+++ tests/notebooks/test_qmc_user_defined.py
@@ -0,0 +1,33 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("qmc_user_defined", timeout_seconds=176)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("model"))
+    validate_quantum_model(tb.ref("model_2"))
+    validate_quantum_model(tb.ref("model_3"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=4,  # actual width: 4
+        expected_depth=150,  # actual depth: 104
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_2"),
+        expected_width=5,  # actual width: 4
+        expected_depth=350,  # actual depth: 236
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_3"),
+        expected_width=7,  # actual width: 7
+        expected_depth=2500,  # actual depth: 2008
+    )
+
+    # test notebook content
+    pass  # TODO
---
+++ tests/notebooks/test_quantum_counting.py
@@ -0,0 +1,27 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("quantum_counting", timeout_seconds=300)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod_qpe"))
+    validate_quantum_model(tb.ref("qmod_iqae"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog_qpe"),
+        expected_width=11,  # actual width: 11
+        expected_depth=8100,  # actual depth: 8058
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_iqae"),
+        expected_width=6,  # actual width: 6
+        expected_depth=500,  # actual depth: 486
+    )
+
+    # test notebook content
+    pass  # TODO
