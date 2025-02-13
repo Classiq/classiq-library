d24975c1ba9f9f6972913108889aa14dd993f574
Dor Harpaz <dor@classiq.io>
2025-02-13 13:38:58 +0200

    Add test: qaoa_knapsack

---
+++ tests/notebooks/test_qaoa_knapsack.py
@@ -0,0 +1,24 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook(
+    "qaoa_knapsack",
+    timeout_seconds=300,
+)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=18,  # actual width: 18
+        expected_depth=2500,  # actual depth: 2323
+    )
+
+    # test notebook content
+    pass  # TODO
