b29b768068cf930ce8ae8176814aa366534826da
Dor Harpaz <dor@classiq.io>
2025-02-13 16:37:51 +0200

    Add test: simon

---
+++ tests/notebooks/test_simon.py
@@ -0,0 +1,48 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+import numpy as np
+
+
+@wrap_testbook(
+    "simon",
+    timeout_seconds=40,
+)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    """
+    A notebook for a hybrid classical quantum neural network.
+    The test verifies that the pre-trained model is indeed well trained.
+    """
+    # test models
+    validate_quantum_model(tb.ref("qmod_1"))
+    validate_quantum_model(tb.ref("qmod_2"))
+    validate_quantum_model(tb.ref("qmod_3"))
+    validate_quantum_model(tb.ref("qmod_4"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog_1"),
+        expected_width=20,  # actual width: 17
+        expected_depth=700,  # actual depth: 576
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_2"),
+        expected_width=20,  # actual width: 17
+        expected_depth=700,  # actual depth: 577
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_3"),
+        expected_width=15,  # actual width: 12
+        expected_depth=10,  # actual depth: 5
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_4"),
+        expected_width=15,  # actual width: 12
+        expected_depth=10,  # actual depth: 6
+    )
+
+    # test notebook content
+    pass  # TODO
