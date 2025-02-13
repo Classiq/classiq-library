d433fcff98a148c53888d3c6ee886833ae6076cc
Dor Harpaz <dor@classiq.io>
2025-02-13 11:52:56 +0200

    Add test: dqi_max_xorsat

---
+++ tests/notebooks/test_dqi_max_xorsat.py
@@ -0,0 +1,38 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook(
+    "dqi_max_xorsat",
+    timeout_seconds=200,
+)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod_one_hot"))
+    validate_quantum_model(tb.ref("qmod_dicke"))
+    validate_quantum_model(tb.ref("qmod"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog_one_hot"),
+        expected_width=16,  # actual width: 16
+        expected_depth=120,  # actual depth: 111
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_dicke"),
+        expected_width=7,  # actual width: 7
+        expected_depth=250,  # actual depth: 223
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=13,  # actual width: 13
+        expected_depth=7250,  # actual depth: 7079
+    )
+
+    # test notebook content
+    assert (
+        tb.ref('sum(sum(sample.state["y"]) for sample in res.parsed_counts)') == 0
+    ), "the y vector is not clean"
