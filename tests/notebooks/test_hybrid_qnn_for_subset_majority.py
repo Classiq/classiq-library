ed2efd9c02d562e7446752a7ce9cf128283964a9
Dor Harpaz <dor@classiq.io>
2025-02-13 13:40:00 +0200

    Update test: hybrid_qnn_for_subset_majority

--- tests/notebooks/test_hybrid_qnn_for_subset_majority.py
+++ tests/notebooks/test_hybrid_qnn_for_subset_majority.py
@@ -15,7 +15,16 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     A notebook for a hybrid classical quantum neural network.
     The test verifies that the pre-trained model is indeed well trained.
     """
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=5,  # actual width: 2
+        expected_depth=10,  # actual depth: 6
+    )
 
+    # test notebook content
     assert (
         tb.ref("accuracy") > 0.85
     ), "The network is not trained, although we load a pre-trained model."
