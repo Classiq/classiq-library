a9745730120602eff1353e5ccde10711e390ca2d
Dor Harpaz <dor@classiq.io>
2025-02-20 17:08:54 +0200

    Add test: task_scheduling_problem

---
+++ tests/notebooks/test_task_scheduling_problem.py
@@ -0,0 +1,27 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("task_scheduling_problem", timeout_seconds=840)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+    validate_quantum_model(tb.ref("qmod_large"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=8,  # actual width: 6
+        expected_depth=120,  # actual depth: 92
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_large"),
+        expected_width=30,  # actual width: 24
+        expected_depth=500,  # actual depth: 428
+    )
+
+    # test notebook content
+    pass  # todo
