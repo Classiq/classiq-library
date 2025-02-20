c7970bffda3d1ad8de8080b290fefad19155f00e
Dor Harpaz <dor@classiq.io>
2025-02-20 15:25:55 +0200

    Use file name

--- tests/test_no_errors.py
+++ tests/test_no_errors.py
@@ -2,12 +2,12 @@ import os
 
 import nbformat
 
-from utils_for_tests import iterate_notebooks
+from utils_for_tests import iterate_notebook_names, resolve_notebook_path
 
 
 def test_no_errors_in_notebooks() -> None:
-    for notebook in iterate_notebooks():
-        with open(notebook) as f:
+    for notebook in iterate_notebook_names():
+        with open(resolve_notebook_path(notebook)) as f:
             notebook_data = nbformat.read(f, nbformat.NO_CONVERT)
 
         for index, cell in enumerate(notebook_data["cells"]):
@@ -17,4 +17,4 @@ def test_no_errors_in_notebooks() -> None:
 
             assert not any(
                 output.get("output_type") == "error" for output in cell["outputs"]
-            ), f"Cell #{index} in {os.path.basename(notebook)} has an output error"
+            ), f"Cell #{index} in {notebook} has an output error"
