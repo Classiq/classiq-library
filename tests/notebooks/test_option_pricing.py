00df90f96f7632f67f68ada0db523504de167717
Dor Harpaz <dor@classiq.io>
2025-02-27 11:40:05 +0200

    Improve test: option_pricing

--- tests/notebooks/test_option_pricing.py
+++ tests/notebooks/test_option_pricing.py
@@ -5,6 +5,8 @@ from tests.utils_for_testbook import (
 )
 from testbook.client import TestbookNotebookClient
 
+import numpy as np
+
 
 @wrap_testbook("option_pricing", timeout_seconds=140)
 def test_notebook(tb: TestbookNotebookClient) -> None:
@@ -18,6 +20,9 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     )
 
     # test notebook content
-    assert tb.ref(
-        "np.isclose(measured_payoff,expected_payoff,atol=10 * (confidence_interval[1] - confidence_interval[0]))"
+    confidence_interval = tb.ref_numpy("confidence_interval")
+    assert np.isclose(
+        tb.ref("measured_payoff"),
+        tb.ref("expected_payoff"),
+        atol=10 * (confidence_interval[1] - confidence_interval[0]),
     )
