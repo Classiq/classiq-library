e2d9638639388ec549365605af115e4d93c29ae7
Dor Harpaz <dor@classiq.io>
2025-02-27 13:54:15 +0200

    Add test: auxiliary_management

---
+++ tests/notebooks/test_auxiliary_management.py
@@ -0,0 +1,21 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("auxiliary_management", timeout_seconds=40)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=10,  # actual width: 8
+        expected_depth=300,  # actual depth: 247
+    )
+
+    # test notebook content
+    pass  # TODO
