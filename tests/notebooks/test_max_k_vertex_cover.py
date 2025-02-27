b1b3885b4517da9727a49bdf3235a2d10d588077
Dor Harpaz <dor@classiq.io>
2025-02-27 12:05:19 +0200

    Add test: max_k_vertex_cover

---
+++ tests/notebooks/test_max_k_vertex_cover.py
@@ -0,0 +1,21 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("max_k_vertex_cover", timeout_seconds=600)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=12,  # actual width: 10
+        expected_depth=200,  # actual depth: 159
+    )
+
+    # test notebook content
+    pass  # Todo
