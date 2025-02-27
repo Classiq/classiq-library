841788e4f9d18fc64dd2b53647f87ab3dd8ec4ee
Dor Harpaz <dor@classiq.io>
2025-02-27 12:27:56 +0200

    fix notebook name

--- tests/notebooks/test_rectangles_packing.py
+++ tests/notebooks/test_rectangles_packing.py
@@ -6,7 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("rectangles_packing", timeout_seconds=1800)
+@wrap_testbook("rectangles_packing_grid", timeout_seconds=1800)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
     validate_quantum_model(tb.ref("qmod"))
