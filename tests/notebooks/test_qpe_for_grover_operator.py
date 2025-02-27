64f939420faff8de5540728df6db362408bfebe9
Dor Harpaz <dor@classiq.io>
2025-02-27 15:25:15 +0200

    Add test: qpe_for_grover_operator

---
+++ tests/notebooks/test_qpe_for_grover_operator.py
@@ -0,0 +1,22 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("qpe_for_grover_operator", timeout_seconds=1000)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # the `qmod`s and `qprog`s are in a for-loop
+    # need to rewrite the notebook in order to test them
+
+    # test notebook content
+    for depth_classiq_optimize_depth, depth_classiq_optimize_width, depth_qiskit in zip(
+        tb.ref("classiq_depths_ae_opt_depth_max_width"),
+        tb.ref("classiq_depths_ae_opt_width"),
+        tb.ref("qiskit_depths_ae"),
+    ):
+        assert (
+            depth_classiq_optimize_depth < depth_classiq_optimize_width < depth_qiskit
+        )
