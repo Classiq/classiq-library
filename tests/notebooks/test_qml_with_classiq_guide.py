603be08be787532b3c6ecea6971fbf7714b5e0ae
Dor Harpaz <dor@classiq.io>
2025-03-06 14:07:31 +0200

    Add test: qml_with_classiq_guide

---
+++ tests/notebooks/test_qml_with_classiq_guide.py
@@ -0,0 +1,28 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("qml_with_classiq_guide", timeout_seconds=300)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod_1"))
+    validate_quantum_model(tb.ref("qmod_2"))
+
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog_1"),
+        expected_width=1,  # actual width: 1
+        expected_depth=1,  # actual depth: 1
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_2"),
+        expected_width=1,  # actual width: 1
+        expected_depth=2,  # actual depth: 2
+    )
+
+    # test notebook content
+    assert tb.ref("check_accuracy(model, data_loader)") == 1
