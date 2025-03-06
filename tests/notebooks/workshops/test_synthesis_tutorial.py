99f32af41e2a0ad4cb0723dfab4f9e860e2426a1
Dor Harpaz <dor@classiq.io>
2025-03-06 14:59:40 +0200

    Add test: synthesis_tutorial

---
+++ tests/notebooks/workshops/test_synthesis_tutorial.py
@@ -0,0 +1,24 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("synthesis_tutorial", timeout_seconds=500)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog_opt_depth"),
+        expected_width=20,  # actual width: 16
+        expected_depth=250,  # actual depth: 171
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_opt_width"),
+        expected_width=20,  # actual width: 9
+        expected_depth=250,  # actual depth: 199
+    )
+
+    # test notebook content
+    pass  # Todo
