ce640e9c35e72e3372e61a9ad6782af7100b0fba
Dor Harpaz <dor@classiq.io>
2025-03-06 16:20:18 +0200

    Add test: analyze

---
+++ tests/notebooks/workshops/test_analyze.py
@@ -0,0 +1,22 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("analyze", timeout_seconds=20)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=15,  # actual width: 12
+        expected_depth=500,  # actual depth: 390
+    )
+
+    # test notebook content
+    pass  # Todo
