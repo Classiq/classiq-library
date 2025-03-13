035a293ecc5d079aabf23605e538033d8efe0a4c
Dor Harpaz <dor@classiq.io>
2025-03-13 14:14:44 +0200

    Add test: combi_workshop_Inequality_constriants_PO

---
+++ tests/notebooks/workshops/test_combi_workshop_Inequality_constriants_PO.py
@@ -0,0 +1,22 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("combi_workshop_Inequality_constriants_PO", timeout_seconds=700)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=13,  # actual width: 9
+        expected_depth=180,  # actual depth: 137
+    )
+
+    # test notebook content
+    pass  # Todo
