9b8b0438210cb00235d482d030dcf1175a22cb0c
Dor Harpaz <dor@classiq.io>
2025-03-13 12:26:03 +0200

    Fix tests

--- tests/notebooks/workshops/test_quantum_variables_and_functions.py
+++ tests/notebooks/workshops/test_quantum_variables_and_functions.py
@@ -15,7 +15,7 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     validate_quantum_program_size(
         tb.ref("qprog"),
         expected_width=8,  # actual width: 6
-        expected_depth=50,  # actual depth: 34
+        expected_depth=70,  # actual depth: 34
     )
 
     # test notebook content
