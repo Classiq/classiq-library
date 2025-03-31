7a42af7ebf0eb67620374fddebf1d7f59b0901f5
Or Samimi Golan <orsa@classiq.io>
2025-03-31 11:30:23 +0300

    cr comments + mis model

--- tests/notebooks/test_max_independent_set.py
+++ tests/notebooks/test_max_independent_set.py
@@ -6,7 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("max_independent_set", timeout_seconds=60)
+@wrap_testbook("max_independent_set", timeout_seconds=200)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
     validate_quantum_model(tb.ref("qmod"))
