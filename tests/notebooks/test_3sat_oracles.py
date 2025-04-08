69d7c2bde0a40fed1e359abf8af025a12f844442
Tomer Goldfriend <tomer@classiq.io>
2025-04-08 12:32:13 +0300

    modify 3sat_oracles for test

--- tests/notebooks/test_3sat_oracles.py
+++ tests/notebooks/test_3sat_oracles.py
@@ -8,9 +8,70 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("3sat_oracles", timeout_seconds=1800)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # the `qmod` and `qprog` are defined in a function
-    # need to rewrite the notebook in order to test it
+
+    num_models = len(tb.ref("qmods"))
+    actual_widths = [
+        25,
+        27,
+        29,
+        32,
+        30,
+        38,
+        40,
+        40,
+        39,
+        40,
+        50,
+        48,
+        53,
+        56,
+        56,
+        62,
+        81,
+        85,
+        104,
+        103,
+        114,
+        150,
+        153,
+    ]
+    for i in range(num_models):
+        # test models
+        validate_quantum_model(tb.ref("qmods"[i]))
+        # test quantum programs
+        validate_quantum_program_size(
+            tb.ref("qprogs"[i]),
+            expected_width=int(
+                actual_widths[i] * 1.5
+            ),  # actual width: actual_widths[i]
+            expected_depth=None,  # actual depth: 429
+        )
 
     # test notebook content
-    pass  # Todo
-    # need to compare classiq times to qiskit times at some num_qubits value
+    rounded_cl_times = [
+        9.0,
+        4.0,
+        4.0,
+        5.0,
+        4.0,
+        5.0,
+        9.0,
+        5.0,
+        5.0,
+        5.0,
+        6.0,
+        5.0,
+        6.0,
+        7.0,
+        5.0,
+        6.0,
+        6.0,
+        8.0,
+        9.0,
+        9.0,
+        9.0,
+        11.0,
+        13.0,
+    ]
+    for i in range(num_models):
+        assert tb.ref("cl_times"[i]) < 2 * rounded_cl_times[i]
