d97307117d7d29afca18da8d6617da54bfb3c595
Tomer Goldfriend <tomer@classiq.io>
2025-04-09 11:02:22 +0300

    modify and test glued trees

--- tests/notebooks/test_glued_trees.py
+++ tests/notebooks/test_glued_trees.py
@@ -8,5 +8,16 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("glued_trees", timeout_seconds=500)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # everything in this test is inside a function, storing results in a file
-    pass  # TODO: need to rewrite the notebook in order to test it
+    actual_widths = [10] * 13 + [20] * 13
+    actual_depths = [1265] * 13 + [1321] * 13
+
+    for qmod in tb.ref("qmods"):
+        # test models
+        validate_quantum_model(qmod)
+    for qprog, e_width, e_depth in zip(tb.ref("qprogs"), actual_widths, actual_depths):
+        # test quantum programs
+        validate_quantum_program_size(
+            qprog,
+            expected_width=int(e_width * 1.5),  # 1.5* actual width
+            expected_depth=int(e_depth * 1.5),  # 1.5* actual depth
+        )
