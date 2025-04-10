e0df06ccefbd74ac68678d67a984e0fd2b15ea7a
Lekha M M <117354716+Lekhamm@users.noreply.github.com>
2025-04-10 12:48:03 +0530

    Update test_pre_commit.py

--- tests/internal/test_pre_commit.py
+++ tests/internal/test_pre_commit.py
@@ -34,9 +34,9 @@ def test_notebooks_unique_names():
     assert notebook_pre_commit_collection.validate_unique_names()
 
 
-
-tests from qmod_pre_commit_collection
-
+#
+# tests from qmod_pre_commit_collection
+#
 @pytest.mark.parametrize("qmod_path", map(str, PROJECT_ROOT.rglob("*.qmod")))
 def test_notebooks(qmod_path: str):
     if "functions/function_declarations" in qmod_path:
