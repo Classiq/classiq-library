fe01f29348c72bf0fc67ffc3c08d1f51e272f13a
Or Samimi Golan <orsa@classiq.io>
2025-04-14 17:40:22 +0300

    fixes

--- tests/notebooks/test_partial_exponential_state_preparation.py
+++ tests/notebooks/test_partial_exponential_state_preparation.py
@@ -6,7 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("partial_exponential_state_preparation", timeout_seconds=60)
+@wrap_testbook("partial_exponential_state_preparation", timeout_seconds=100)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
     validate_quantum_model(tb.ref("qmod"))
