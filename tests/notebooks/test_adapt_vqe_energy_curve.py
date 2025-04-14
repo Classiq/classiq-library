7ef9a3f458e93a1bdd3fbc6204dabb39e4127c09
Dacastillo <dacastillo@uc.cl>
2025-04-13 21:59:13 -0400

    cleaning and code adding 3

--- tests/notebooks/test_adapt_vqe_energy_curve.py
+++ tests/notebooks/test_adapt_vqe_energy_curve.py
@@ -5,27 +5,20 @@ from tests.utils_for_testbook import (
 )
 from testbook.client import TestbookNotebookClient
 
-import numpy as np
-
-
 @wrap_testbook("adapt_vqe_energy_curve", timeout_seconds=300)
 def test_notebook(tb: TestbookNotebookClient) -> None:
+    """
+    A notebook for a hybrid classical quantum neural network.
+    The test verifies that the pre-trained model is indeed well trained.
+    """
     # test models
     for qmod in tb.ref("qmods"):
         validate_quantum_model(qmod)
     # test quantum programs
-    #for qprog in tb.ref("qprogs"):
-    #    validate_quantum_program_size(
-    #        qprog,
-    #        expected_width=16,  # actual width: 16
-    #        expected_depth=12000,  # actual depth: 11538
-    #    )
-
+    validate_quantum_program_size(
+        tb.ref("qprog"),
+        expected_width=1,  # actual width: 1
+        expected_depth=10,  # actual depth: 4
+    )
     # test notebook content
-    #for duration in tb.ref("durations"):
-    #    assert duration < 40  # locally it is no longer than 15 seconds
-
-    #assert all(
-    #    np.isclose(tb.ref("exact_energy"), tb.ref("aVQE_energy"), atol=0.02)
-    #)  # should be atol=0.01
-    pass
+    pass  # TODO
