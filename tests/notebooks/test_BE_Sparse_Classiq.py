75f9add56c19bd8a90c3412b5a9e14b06efe0348
bit2qbit <virajdanieldsouza@gmail.com>
2025-04-23 22:20:35 +0530

    test_edits

--- tests/notebooks/test_BE_Sparse_Classiq.py
+++ tests/notebooks/test_BE_Sparse_Classiq.py
@@ -6,16 +6,39 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("BE_Sparse_Classiq.ipynb", timeout_seconds=3000)
+@wrap_testbook("BE_Sparse_Classiq.ipynb", timeout_seconds=1000)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
-    validate_quantum_model(tb.ref("qmod"))
+    validate_quantum_model(tb.ref("qmod_ebt"))
+    validate_quantum_model(tb.ref("qmod_ebt_lcu"))
+    validate_quantum_model(tb.ref("qmod_sbc"))
+    validate_quantum_model(tb.ref("qmod_md"))
+    validate_quantum_model(tb.ref("qmod_nmd"))
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref("qprog"),
+        tb.ref("qprog_ebt"),
         expected_width=None,
         expected_depth=None,
     )
-
+    validate_quantum_program_size(
+        tb.ref("qprog_ebt_lcu"),
+        expected_width=None,
+        expected_depth=None,
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_sbc"),
+        expected_width=None,
+        expected_depth=None,
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_md"),
+        expected_width=None,
+        expected_depth=None,
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_ebt_nmd"),
+        expected_width=None,
+        expected_depth=None,
+    )
     # test notebook content
     pass  # Todo
