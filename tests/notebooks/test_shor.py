c423d126f535321c65357026717f17861b927e93
Or Samimi Golan <orsa@classiq.io>
2025-04-27 11:55:48 +0300

    fixed quantum counting, shor

--- tests/notebooks/test_shor.py
+++ tests/notebooks/test_shor.py
@@ -6,16 +6,28 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("shor", timeout_seconds=104)
+@wrap_testbook("shor", timeout_seconds=300)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
-    validate_quantum_model(tb.ref("qmod"))
+    validate_quantum_model(tb.ref("qmod_1"))
+    validate_quantum_model(tb.ref("qmod_2"))
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref("qprog"),
-        expected_width=12,  # actual width: 12
-        expected_depth=1111,  # actual depth: 1099
+        tb.ref("qprog_1"),
+        expected_width=8,  # actual width: 8
+        expected_depth=300,  # actual depth: 296
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_2"),
+        expected_width=22,  # actual width: 22
+        expected_depth=30000,  # actual depth: 26607
     )
 
     # test notebook content
-    pass  # TODO
+    counts_results = tb.ref("result_2")
+    d = {
+        item.state["pow"]: item.shots
+        for item in counts_results.parsed_counts_of_outputs("pow")
+    }
+    for val in [0, 171, 341, 512, 683, 853]:
+        assert val in d and d[val] > 120, "Unexpected shor execution results"
