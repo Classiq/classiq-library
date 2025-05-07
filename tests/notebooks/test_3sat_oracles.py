b717bd566645d587ea7bc6fd6d236016a5079b5c
Tomer Goldfriend <tomer@classiq.io>
2025-05-07 23:58:22 +0300

    update 3sat test

--- tests/notebooks/test_3sat_oracles.py
+++ tests/notebooks/test_3sat_oracles.py
@@ -8,10 +8,42 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("3sat_oracles", timeout_seconds=1800)
 def test_notebook(tb: TestbookNotebookClient) -> None:
+    expected_widths = [
+        25,
+        25,
+        27,
+        33,
+        35,
+        34,
+        36,
+        37,
+        40,
+        48,
+        47,
+        45,
+        53,
+        55,
+        64,
+        72,
+        70,
+        82,
+        100,
+        101,
+        110,
+        118,
+        136,
+        137,
+    ]
 
     for qmod in tb.ref("qmods"):
         validate_quantum_model(qmod)
-        # TODO test quantum programs with transpilation is "none"
+    for qprog, expected_width in zip(tb.ref("qprogs"), expected_widths):
+        validate_quantum_program_size(
+            qprog,
+            expected_width=expected_width + 5,  # actual width + 5
+            expected_depth=None,  # actual depth: 0
+            allow_zero_size=True,
+        )
 
     # test notebook content
     for cl_time in tb.ref("cl_times"):
