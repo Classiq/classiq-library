5a470a41c0ac4ec28863c84b29e5b030149e236e
Nadav Ben Ami <nadav.ba@classiq.io>
2025-05-21 13:47:42 +0300

    fix cyber notbooks naming for docs

--- tests/notebooks/test_patch_min_vertex_cover.py
+++ tests/notebooks/test_patch_min_vertex_cover.py
@@ -6,7 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("patching_managment", timeout_seconds=36)
+@wrap_testbook("patch_min_vertex_cover", timeout_seconds=36)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
     validate_quantum_model(tb.ref("qmod"))
