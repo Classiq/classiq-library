c268f11aa870b2216a80f78d7d58bfce387bd468
Alexandre-Classiq <alexandre@classiq.io>
2025-05-21 11:14:41 -0300

    Minor changes

--- tests/notebooks/test_execution_tutorial_part2.py
+++ tests/notebooks/test_execution_tutorial_part2.py
@@ -5,17 +5,27 @@ from tests.utils_for_testbook import (
 )
 from testbook.client import TestbookNotebookClient
 
+import numpy as np
+
 
 @wrap_testbook("execution_tutorial_part2", timeout_seconds=60)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
-    validate_quantum_model(tb.ref("qmod"))
+    validate_quantum_model(tb.ref("execution_tutorial_part2"))
+    validate_quantum_model(tb.ref("execution_tutorial_part2_bell"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref("qprog"),
-        expected_width=None,
-        expected_depth=None,
+        expected_width=2,
+        expected_depth=3,
+    )
+    validate_quantum_program_size(
+        tb.ref("qprog_bell"),
+        expected_width=2,
+        expected_depth=3,
     )
 
     # test notebook content
-    pass  # Todo
+    first_estimate = tb.ref("first_estimate")
+    tolerance = 0.1 #This value can be reduced if we increase the number of shots on the execution preferences.
+    np.isclose(first_estimate, 0.20, atol=tolerance)
