3f5ff620a5011e279cea90bf5c4865681d0ed417
Seungmin Jeon <seungmin.jeon@cp.kaist.ac.kr>
2025-05-21 22:54:17 +0900

    Change timeout

--- tests/notebooks/test_stateprep_guassian_using_qsvt.py
+++ tests/notebooks/test_stateprep_guassian_using_qsvt.py
@@ -6,7 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("stateprep_guassian_using_qsvt", timeout_seconds=60)
+@wrap_testbook("stateprep_guassian_using_qsvt", timeout_seconds=600)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
     validate_quantum_model(tb.ref("qmod"))
