181db2cbc0795c160f89782facd3100ae718902d
Or Samimi Golan <orsa@classiq.io>
2025-06-17 22:42:16 +0300

    updated notebooks

--- tests/notebooks/test_grover_max_cut.py
+++ tests/notebooks/test_grover_max_cut.py
@@ -8,8 +8,6 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("grover_max_cut", timeout_seconds=220)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
