612f211b6fc3ed0b652b900613db6c1aa3505df6
Dor Harpaz <dor@classiq.io>
2025-06-19 16:41:31 +0300

    Update test - rainbow_options_direct_method

--- tests/notebooks/test_rainbow_options_direct_method.py
+++ tests/notebooks/test_rainbow_options_direct_method.py
@@ -8,4 +8,26 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("rainbow_options_direct_method", timeout_seconds=1000)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    pass  # TODO
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=tb.ref("MAX_WIDTH"),
+        expected_depth=5000,  # actual depth: 4320
+    )
+
+    # test notebook content
+    parsed_result = tb.ref("parsed_result")
+    conf_interval = tb.ref("conf_interval")
+    ALPHA_VALUE = tb.ref("ALPHA_VALUE")
+    expected_payoff = 23.0238
+    ALPHA_ASSERTION = 1e-5
+    measured_confidence = conf_interval[1] - conf_interval[0]
+    confidence_scale_by_alpha = np.sqrt(
+        np.log(ALPHA_VALUE / ALPHA_ASSERTION)
+    )  # based on e^2=(1/2N)*log(2T/alpha) from "Iterative Quantum Amplitude Estimation" since our alpha is low, we want to check within a bigger confidence interval
+    assert (
+        np.abs(parsed_result - expected_payoff)
+        <= 0.5 * measured_confidence * confidence_scale_by_alpha
+    ), f"Payoff result is out of the {ALPHA_ASSERTION*100}% confidence interval: |{parsed_result} - {expected_payoff}| > {0.5*measured_confidence * confidence_scale_by_alpha}"
