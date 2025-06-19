2f837444042a8b19c5e8ba0f52f195e2c76fdabd
Dor Harpaz <dor@classiq.io>
2025-06-19 17:00:15 +0300

    Update test - time_marching

--- tests/notebooks/test_time_marching.py
+++ tests/notebooks/test_time_marching.py
@@ -8,4 +8,26 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("time_marching", timeout_seconds=500)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    pass  # TODO
+    # test models
+    validate_quantum_model(tb.ref("qmod"))
+    validate_quantum_model(tb.ref("qmod_naive"))
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=20,  # actual width: 16
+        expected_depth=22000,  # actual depth: 18687
+    )
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_naive"),
+        expected_width=20,  # actual width: 16
+        expected_depth=3500,  # actual depth: 2913
+    )
+
+    # test notebook content
+    for amplitude_amplified, amplitude_naive in zip(
+        tb.ref("amplitudes_amplified"), tb.ref("amplitudes_naive")
+    ):
+        # should be a factor of 10 larger. taking a factor of 8 as some buffer.
+        assert amplitude_naive < amplitude_amplified / 8
+
+    assert tb.ref("np.linalg.norm(sampled - expected)") < 0.1
