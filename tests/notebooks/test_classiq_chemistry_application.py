bab850ff8d0922b7138f6a19b53e85d15cf1adb3
Tomer Goldfriend <tomer@classiq.io>
2025-06-26 11:44:16 +0300

    add new test

--- tests/notebooks/test_classiq_chemistry_application.py
+++ tests/notebooks/test_classiq_chemistry_application.py
@@ -1,6 +1,5 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -8,21 +7,20 @@ import numpy as np
 
 
 @wrap_testbook(
-    "molecule_eigensolver_using_openfermion",
-    timeout_seconds=600,
+    "classiq_chemistry_application",
+    timeout_seconds=80,
 )
 def test_notebook(tb: TestbookNotebookClient) -> None:
     """
     A notebook for finding the ground state of a molecule using VQE.
     The test verifies that the classical optimizer converges to the expected ground state.
     """
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
+
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
-        expected_width=8,  # actual width: 8
-        expected_depth=650,  # actual depth: 484
+        expected_width=6,  # actual width: 8
+        expected_depth=300,  # actual depth: 166
     )
 
     # test notebook content
