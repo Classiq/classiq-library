02dc21d1f67475bbc9f99c2e23fec6a6a6e1a0e6
Or Samimi Golan <orsa@classiq.io>
2025-07-02 15:08:38 +0300

    fixed tests

--- tests/notebooks/test_min_graph_coloring.py
+++ tests/notebooks/test_min_graph_coloring.py
@@ -14,7 +14,7 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
         expected_width=17,  # actual width: 15
-        expected_depth=5000,  # actual depth: 4543
+        expected_depth=7500,  # actual depth: 6273
     )
 
     # test notebook content
