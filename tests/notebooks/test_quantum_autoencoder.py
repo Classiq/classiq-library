7f63511ae527a6a812a071902ac7086f7c502abb
Tomer Goldfriend <tomer@classiq.io>
2025-07-03 09:17:51 +0300

    with ae metadata and test

--- tests/notebooks/test_quantum_autoencoder.py
+++ tests/notebooks/test_quantum_autoencoder.py
@@ -12,20 +12,17 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     A notebook for a hybrid classical quantum neural network.
     The test verifies that the pre-trained model is indeed well trained.
     """
-    # test models
-    validate_quantum_model(tb.ref("ae_qmod"))
-    validate_quantum_model(tb.ref("qmod_validator"))
-    validate_quantum_model(tb.ref("qmod_ae_alt"))
+
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
+        tb.ref_pydantic("qprog_ae_network"),
         expected_width=10,  # actual width: 7
         expected_depth=50,  # actual depth: 30
     )
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_validator"),
-        expected_width=10,  # actual width: 6
-        expected_depth=20,  # actual depth: 13
+        expected_width=8,  # actual width: 4
+        expected_depth=28,  # actual depth: 18
     )
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_ae_alt"),
@@ -34,7 +31,7 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     )
 
     # test notebook content
-    for input_, output in tb.ref(
-        "[(data.tolist()[0] , validator_network(data).tolist()[0]) for data, _ in validator_data_loader]"
-    ):
-        assert input_ == output, "autoencoder failed to encode"
+    for data, res in zip(tb.ref("input_data"), tb.ref_pydantic("results_validator")):
+        df = res.dataframe
+        output = df.loc[df["probability"].idxmax(), "decoded"]
+        assert data == output, "autoencoder failed to encode"
