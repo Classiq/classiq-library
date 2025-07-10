433957e00a01010451cf01999e0dc83283cdbd68
Dor Harpaz <dor@classiq.io>
2025-07-10 11:37:30 +0300

    Expand error message on notebook without test

--- tests/test_all_notebooks_are_tested.py
+++ tests/test_all_notebooks_are_tested.py
@@ -7,9 +7,12 @@ from utils_for_tests import get_all_notebooks, ROOT_DIRECTORY, resolve_notebook_
 def test_is_notebook_tested(notebook_name: str):
     if not _should_skip_notebook(notebook_name):
         expected_test_name = f"test_{notebook_name[:-6]}.py"  # [:-6] removes ".ipynb"
-        assert list(
-            ROOT_DIRECTORY.rglob(expected_test_name)
-        ), f"No test was found for '{notebook_name}'. Expecting to find '{expected_test_name}'"
+        assert list(ROOT_DIRECTORY.rglob(expected_test_name)), (
+            f"No test was found for '{notebook_name}'. Expecting to find '{expected_test_name}'"
+            f"\nIt is common for notebooks tests to be placed in 'ROOT_FOLDER/tests/notebooks/{expected_test_name}'."
+            "\nNote: tests are auto-generated when using pre-commit."
+            "\nIf you prefer to generate them without pre-commit, please execute 'ROOT_FOLDER/.internal/pre_commit_tools/auto_add_tests.py <full_path_to_your_new_notebook_ipynb_file>'"
+        )
 
 
 def _should_skip_notebook(notebook_name: str) -> bool:
