45c2194d6a7bf239489121545f29d52d1bd623d8
Alexandre-Classiq <alexandre@classiq.io>
2025-07-14 15:39:18 -0300

    Modifying test notebook

--- tests/notebooks/test_execution_tutorial.py
+++ tests/notebooks/test_execution_tutorial.py
@@ -17,5 +17,5 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_IBM"),
         expected_width=17,  # actual width: 9-16
-        expected_depth=1100,  # actual depth: 900 - 1000
+        expected_depth=1500,  # actual depth: 900 - 1000
     )
