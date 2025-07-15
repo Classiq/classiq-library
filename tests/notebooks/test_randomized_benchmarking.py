698b4402f7ef40151a50b1b10b75fdee63c8ed12
Or Samimi Golan <orsa@classiq.io>
2025-07-15 10:41:55 +0300

    removed errornous test

--- tests/notebooks/test_randomized_benchmarking.py
+++ tests/notebooks/test_randomized_benchmarking.py
@@ -8,14 +8,4 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("randomized_benchmarking", timeout_seconds=60)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
-    # test quantum programs
-    validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
-    )
-
-    # test notebook content
-    pass  # Todo
+    pass
