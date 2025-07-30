95a9a57857763ea764e3c8d148964f5ab430327c
Tomer Goldfriend <tomer@classiq.io>
2025-07-30 14:03:04 +0300

    test qpe

--- tests/notebooks/test_qpe_for_matrix.py
+++ tests/notebooks/test_qpe_for_matrix.py
@@ -9,17 +9,23 @@ from testbook.client import TestbookNotebookClient
 @wrap_testbook("qpe_for_matrix", timeout_seconds=748)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     """
-    A notebook for a hybrid classical quantum neural network.
-    The test verifies that the pre-trained model is indeed well trained.
+    A notebook for a qpe for matrix.
     """
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
-    # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=12,  # actual width: 10
-        expected_depth=500,  # actual depth: 443
+        tb.ref_pydantic("qprog_exact"),
+        expected_width=9,  # actual width: 9
+        expected_depth=8000,  # actual depth: 6144
+    )
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_approx"),
+        expected_width=9,  # actual width: 9
+        expected_depth=32000,  # actual depth: 26311
     )
 
     # test notebook content
-    pass  # TODO
+    w = tb.ref_pydantic("w")
+    width = tb.ref_pydantic("energy_resolution")
+    for s in tb.ref_pydantic("solution_exact"):
+        assert any(abs(s - wi) <= width for wi in w)
+    for s in tb.ref_pydantic("solution_approx"):
+        assert any(abs(s - wi) <= width for wi in w)
