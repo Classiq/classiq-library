2540f6562b6ef231c1965e8348a6c4a075f0e2fb
Tomer Goldfriend <tomer@classiq.io>
2025-07-31 21:41:19 +0300

    with vqls

--- tests/notebooks/test_vqls_with_lcu.py
+++ tests/notebooks/test_vqls_with_lcu.py
@@ -1,6 +1,5 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -9,13 +8,8 @@ from testbook.client import TestbookNotebookClient
 @wrap_testbook("vqls_with_lcu", timeout_seconds=1200)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     """
-    A notebook for a hybrid classical quantum neural network.
-    The test verifies that the pre-trained model is indeed well trained.
+    A notebook for variational quantum solver.
     """
-    # test models
-    validate_quantum_model(tb.ref("qmod_1"))
-    validate_quantum_model(tb.ref("qmod_2"))
-    validate_quantum_model(tb.ref("qmod_3"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_1"),
