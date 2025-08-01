01b66e78cf85390c9f30e7baaed4edb79c00f0db
Tomer Goldfriend <tomer@classiq.io>
2025-08-01 07:47:08 +0300

    with sp approx tech_demo

--- tests/notebooks/classiq_paper/test_classiq_discrete_quantum_walk.py
+++ tests/notebooks/classiq_paper/test_classiq_discrete_quantum_walk.py
@@ -1,6 +1,5 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -8,8 +7,6 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("classiq_discrete_quantum_walk", timeout_seconds=300)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
