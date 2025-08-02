261d92f38a8054d25960fb59655f843c90c21374
Tomer Goldfriend <tomer@classiq.io>
2025-08-02 21:45:08 +0300

    with hamiltonian sim tech_demo

--- tests/notebooks/test_hamiltonian_evolution.py
+++ tests/notebooks/test_hamiltonian_evolution.py
@@ -1,12 +1,11 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("hamiltonian_evolution", timeout_seconds=1200)
+@wrap_testbook("hamiltonian_evolution", timeout_seconds=2400)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
     for qprog in tb.ref_pydantic("all_qprogs"):
