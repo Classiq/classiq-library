86b1fb9bf4079da95399b45f3f84c2221781c033
Or Samimi Golan <orsa@classiq.io>
2025-08-03 16:06:13 +0300

    fixed test

--- tests/notebooks/test_patch_min_vertex_cover.py
+++ tests/notebooks/test_patch_min_vertex_cover.py
@@ -14,7 +14,7 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
         expected_width=10,  # actual width: 8
-        expected_depth=35,  # actual depth: 26
+        expected_depth=90,  # actual depth: 79
     )
 
     # test notebook content
