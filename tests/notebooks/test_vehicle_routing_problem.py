a1b99a479d9b94393fc4b5ccb1be532b53e4dfc8
TamuzDanzig1 <118278794+TamuzDanzig1@users.noreply.github.com>
2025-08-06 10:08:46 +0300

    Add tests to VRP notebook

---
+++ tests/notebooks/test_vehicle_routing_problem.py
@@ -0,0 +1,18 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    validate_quantum_model,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("vehicle_routing_problem", timeout_seconds=500)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=20,  # actual width: 16
+    )
+
+    # test notebook content
+    pass  # Todo
