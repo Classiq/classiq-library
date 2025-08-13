abcd56609b9a665658da9aa7e8b6bf98f19cb97f
ravidalon <ravid@classiq.io>
2025-08-13 09:40:00 +0300

    Use pathlib over os
    Signed-off-by: ravidalon <ravid@classiq.io>

--- .internal/app-store/mkdocs-plugins/app_store_plugins/jupyter_to_markdown.py
+++ .internal/app-store/mkdocs-plugins/app_store_plugins/jupyter_to_markdown.py
@@ -1,6 +1,4 @@
 import concurrent.futures
-import os
-from collections.abc import Iterator
 from pathlib import Path
 import shutil
 from tempfile import TemporaryDirectory
@@ -20,20 +18,14 @@ APP_STORE_DOCS_DIR = REPO_ROOT / ".internal" / "app-store" / "docs"
 DIRECTORIES_TO_COPY = ["algorithms", "applications", "tutorials", "community"]
 
 
-def jupyter_notebooks(path: str) -> Iterator[str]:
-    for dirpath, _, filenames in os.walk(path):
-        for filename in filenames:
-            if filename.endswith(".ipynb"):
-                yield os.path.join(dirpath, filename)
-
-
 class JupyterToMarkdown(BasePlugin):
-    def _convert_notebook(self, notebook_path: str, tmpdir: str) -> None:
-        src_path = Path(notebook_path)
-        markdown, resources = self._create_markdown_and_resources(src_path, tmpdir)
-        dest_dir = (APP_STORE_DOCS_DIR / src_path.relative_to(tmpdir)).parent
+    def _convert_notebook(self, full_path: Path, relative_path: Path) -> None:
+        markdown, resources = self._create_markdown_and_resources(
+            full_path, relative_path
+        )
+        dest_dir = (APP_STORE_DOCS_DIR / relative_path).parent
         dest_dir.mkdir(parents=True, exist_ok=True)
-        dest_md = (dest_dir / src_path.name).with_suffix(".md")
+        dest_md = (dest_dir / full_path.name).with_suffix(".md")
         dest_md.write_text(markdown)
         for filename, data in resources.items():
             if isinstance(data, str):
@@ -43,27 +35,26 @@ class JupyterToMarkdown(BasePlugin):
 
     @staticmethod
     def _create_markdown_and_resources(
-        notebook_path: Path, tmpdir: str
+        full_path: Path, relative_path: Path
     ) -> tuple[str, MarkdownResources]:
-        with open(notebook_path) as f:
+        with open(full_path) as f:
             notebook: NotebookNode = read_notebook(f, as_version=4)
         fix_notebook(notebook)
         exporter: MarkdownExporter = MarkdownExporter()
         markdown, resources = exporter.from_notebook_node(notebook)
-        markdown = add_external_links_to_markdown(
-            markdown, notebook_path.relative_to(tmpdir)
-        )
+        markdown = add_external_links_to_markdown(markdown, relative_path)
         return markdown, resources["outputs"]
 
     def on_pre_build(self, config: MkDocsConfig) -> None:
         with concurrent.futures.ProcessPoolExecutor() as executor, TemporaryDirectory() as tmpdir:
+            tmpdir_path = Path(tmpdir)
             for dir_to_copy in DIRECTORIES_TO_COPY:
-                shutil.copytree(
-                    REPO_ROOT / dir_to_copy, os.path.join(tmpdir, dir_to_copy)
-                )
+                shutil.copytree(REPO_ROOT / dir_to_copy, tmpdir_path / dir_to_copy)
             futures = [
-                executor.submit(self._convert_notebook, notebook, tmpdir)
-                for notebook in jupyter_notebooks(tmpdir)
+                executor.submit(
+                    self._convert_notebook, notebook, notebook.relative_to(tmpdir_path)
+                )
+                for notebook in tmpdir_path.rglob("*.ipynb")
             ]
 
             for future in concurrent.futures.as_completed(futures):
