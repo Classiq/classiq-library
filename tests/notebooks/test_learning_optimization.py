5bce50e1206f90a857ad0b1d5413fa07bda718a9
Or Samimi Golan <orsa@classiq.io>
2025-08-13 16:14:32 +0300

    update learning optimization notebook to the new functionality

--- tests/notebooks/test_learning_optimization.py
+++ tests/notebooks/test_learning_optimization.py
@@ -7,21 +7,15 @@ from testbook.client import TestbookNotebookClient
 
 
 @wrap_testbook(
-    "learning_optimization", timeout_seconds=200
+    "learning_optimization", timeout_seconds=100
 )  # 2025.03.06 bump from 80 seconds
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
-    validate_quantum_model(tb.ref("qmod_1"))
-    validate_quantum_model(tb.ref("qmod_2"))
+    validate_quantum_model(tb.ref("qmod"))
 
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog_1"),
-        expected_width=10,  # actual width: 8
-        expected_depth=60,  # actual depth: 44
-    )
-    validate_quantum_program_size(
-        tb.ref_pydantic("qprog_2"),
+        tb.ref_pydantic("qprog"),
         expected_width=10,  # actual width: 8
         expected_depth=60,  # actual depth: 44
     )
