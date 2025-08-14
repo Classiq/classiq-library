1c9b85ecf9195107362c3540174016d833bdf122
arielsmoler <ariel@classiq.io>
2025-08-14 15:01:43 +0300

    Update test_elliptic_curve_discrete_log.py

--- tests/notebooks/test_elliptic_curve_discrete_log.py
+++ tests/notebooks/test_elliptic_curve_discrete_log.py
@@ -9,10 +9,10 @@ from testbook.client import TestbookNotebookClient
 @wrap_testbook("elliptic_curve_discrete_log", timeout_seconds=1800)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
-    # validate_quantum_model(tb.ref("qmod"))
+    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref("qprog"),
+        tb.ref_pydantic("qprog"),
         expected_width=None,
         expected_depth=None,
     )
