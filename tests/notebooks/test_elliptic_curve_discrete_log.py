168cdff7eff5dab45a9d79b923209470e0e2210f
arielsmoler <ariel@classiq.io>
2025-08-15 18:21:49 +0300

    Format and update ecdlp files and test_elliptic_curve_discrete_log.py

--- tests/notebooks/test_elliptic_curve_discrete_log.py
+++ tests/notebooks/test_elliptic_curve_discrete_log.py
@@ -12,9 +12,9 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
+        tb.ref_pydantic("qprog_shor_ecdlp"),
+        expected_width=25,
+        expected_depth=100000,
     )
 
     # test notebook content
