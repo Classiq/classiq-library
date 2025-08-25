fc0657f6865c1679ecedee07141c013bc2ef85f4
Zachary Scott-Murphy <77255406+Aukau@users.noreply.github.com>
2025-08-24 20:59:36 -0700

    Update test_qGAN_challenge.py

--- tests/notebooks/test_qGAN_challenge.py
+++ tests/notebooks/test_qGAN_challenge.py
@@ -8,14 +8,5 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("qGAN_challenge", timeout_seconds=60)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
-    # test quantum programs
-    validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
-    )
-
     # test notebook content
     pass  # Todo
