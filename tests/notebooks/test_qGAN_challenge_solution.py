6fe9bb2c1ea1c3b3c1bd31df4a321fb55474e181
Zachary Scott-Murphy <77255406+Aukau@users.noreply.github.com>
2025-08-24 19:09:30 -0700

    Update test_qGAN_challenge_solution.py

--- tests/notebooks/test_qGAN_challenge_solution.py
+++ tests/notebooks/test_qGAN_challenge_solution.py
@@ -1,12 +1,12 @@
-import os
-import pytest
 from pathlib import Path
-from classiq_library.tests_helpers.run_notebook import run_notebook
+import pytest
+from tests_helpers.run_notebook import run_notebook
 
-ROOT = Path(__file__).resolve().parents[3]  # repo root
+TESTS_DIR = Path(__file__).resolve().parents[1]
+REPO_ROOT = TESTS_DIR.parent
 
 @pytest.mark.timeout(600)
 def test_qGAN_challenge_solution():
-    nb_path = ROOT / "tutorials" / "basic_tutorials" / "qGAN-state-prep" / "Solution/qGAN_challenge_solution.ipynb"
+    nb_path = REPO_ROOT / "tutorials" / "basic_tutorials" / "qGAN-state-prep" / "Solution" / "qGAN_challenge_solution.ipynb"
     assert nb_path.exists(), f"Notebook not found: {nb_path}"
     run_notebook(nb_path)
