f6b407e9ecb930afb6549fa4d63262bd8fee637f
ravidalon <ravid@classiq.io>
2025-08-27 10:40:11 +0300

    Reduce pre-commit runtime
    Signed-off-by: ravidalon <ravid@classiq.io>

--- .internal/pre_commit_tools/clean_timeouts.py
+++ .internal/pre_commit_tools/clean_timeouts.py
@@ -12,14 +12,7 @@ TIMEOUTS_FILE = PROJECT_ROOT / "tests" / "resources" / "timeouts.yaml"
 
 
 def main() -> bool:
-    return validate_unique_keys() and remove_missing_files()
-
-
-def validate_unique_keys() -> bool:
-    with TIMEOUTS_FILE.open("r") as f:
-        data = f.read()
-
-    keys = [line.split(":")[0].strip() for line in data.splitlines()]
+    keys = _get_all_timeout_keys()
     duplicate_keys = [key for key, count in Counter(keys).items() if count > 1]
     if duplicate_keys:
         print(
@@ -27,42 +20,38 @@ def validate_unique_keys() -> bool:
             f"    Please open '{str(TIMEOUTS_FILE)}' and remove one of the duplicate keys.\n"
             f"    The duplicate keys are: ({duplicate_keys})"
         )
+        return False
 
-    return not duplicate_keys
-
-
-def find_missing_files() -> list[str]:
-    with TIMEOUTS_FILE.open("r") as f:
-        timeouts = yaml.safe_load(f)
-
-    missing_files = list(
-        filter(
-            lambda file_name: not any(PROJECT_ROOT.rglob(file_name)),
-            timeouts,
-        )
-    )
-    return missing_files
-
-
-def remove_missing_files() -> bool:
-    missing_files = find_missing_files()
+    all_files = {p.name for p in PROJECT_ROOT.rglob("*") if p.is_file()}
+    missing_files = {key for key in keys if key not in all_files}
     if missing_files:
         print(
             "While looking at the `timeouts.yaml` file, some old keys were found.\n"
             "    These keys point to notebooks/qmods which are long gone.\n"
             "    We'll automatically remove them. Make sure to `git add` these new changes."
         )
-        with TIMEOUTS_FILE.open("r") as f:
-            timeouts = yaml.safe_load(f)
+        _remove_keys(missing_files)
+        return False
 
-        for file in missing_files:
-            del timeouts[file]
+    return True
 
-        with open(TIMEOUTS_FILE, "w") as f:
-            yaml.dump(timeouts, f, sort_keys=True)
 
-    is_ok = not missing_files
-    return is_ok
+def _get_all_timeout_keys() -> list[str]:
+    with TIMEOUTS_FILE.open("r") as f:
+        data = f.read()
+
+    return [line.split(":")[0].strip() for line in data.splitlines()]
+
+
+def _remove_keys(keys: set[str]) -> None:
+    with TIMEOUTS_FILE.open("r") as f:
+        timeouts = yaml.safe_load(f)
+
+    for key in keys:
+        del timeouts[key]
+
+    with open(TIMEOUTS_FILE, "w") as f:
+        yaml.dump(timeouts, f, sort_keys=True)
 
 
 if __name__ == "__main__":
