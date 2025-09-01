b806a0bccdd24805d8fcce6f952681a5a6cd323e
TamuzDanzig1 <118278794+TamuzDanzig1@users.noreply.github.com>
2025-09-01 13:48:01 +0300

    Example

---
+++ tests/notebooks/test_automotive_example.py
@@ -0,0 +1,18 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("automotive_example", timeout_seconds=60)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=None,
+        expected_depth=None,
+    )
+
+    # test notebook content
+    pass  # Todo
