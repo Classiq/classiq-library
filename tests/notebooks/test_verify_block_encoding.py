495dddad2536236adb2c139690e509946a79492b
Tomer Goldfriend <tomer@classiq.io>
2025-09-02 23:44:03 +0300

    update tests

--- tests/notebooks/test_verify_block_encoding.py
+++ tests/notebooks/test_verify_block_encoding.py
@@ -5,14 +5,26 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("verify_block_encoding", timeout_seconds=60)
+@wrap_testbook("verify_block_encoding", timeout_seconds=120)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
+        tb.ref_pydantic("qprog_pauli_be"),
+        expected_width=10,  # actual 8
+        expected_depth=800,  # actual 416
     )
-
-    # test notebook content
-    pass  # Todo
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_banded_be"),
+        expected_width=15,  # actual 10
+        expected_depth=1400,  # actual 834
+    )
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_pauli_sym_be"),
+        expected_width=11,  # actual 9
+        expected_depth=900,  # actual 493
+    )
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_banded_sym_be"),
+        expected_width=16,  # actual 9
+        expected_depth=3000,  # actual 1327
+    )
