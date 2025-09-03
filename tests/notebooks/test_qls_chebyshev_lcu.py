e8da96d00acb361179a671c3d55a2ecb4f5e9bda
Tomer Goldfriend <tomer@classiq.io>
2025-09-03 11:24:18 +0300

    add chebyshev solver

--- tests/notebooks/test_qls_chebyshev_lcu.py
+++ tests/notebooks/test_qls_chebyshev_lcu.py
@@ -5,15 +5,16 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("qls_chebyshev_lcu", timeout_seconds=60)
+@wrap_testbook("qls_chebyshev_lcu", timeout_seconds=800)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
+        tb.ref_pydantic("qprog_cheb_lcu_banded"),
+        expected_width=20,  # actual 17
+        expected_depth=120000,  # actual 60330
     )
-
-    # test notebook content
-    pass  # Todo
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_cheb_lcu_pauli"),
+        expected_width=17,  # actual 15
+        expected_depth=50000,  # actual 17963
+    )
