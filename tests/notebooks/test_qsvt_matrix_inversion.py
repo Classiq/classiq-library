3f3200315f50e60f5a86a9c8f9fd316b18c462f5
Tomer Goldfriend <tomer@classiq.io>
2025-09-10 10:50:13 +0300

    update test

--- tests/notebooks/test_qsvt_matrix_inversion.py
+++ tests/notebooks/test_qsvt_matrix_inversion.py
@@ -1,6 +1,5 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -14,11 +13,19 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     A notebook for a hybrid classical quantum neural network.
     The test verifies that the pre-trained model is indeed well trained.
     """
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
         expected_width=5,  # actual width: 4
-        expected_depth=1500,  # actual depth: 1328
+        expected_depth=3200,  # actual depth: 2150
+    )
+
+    computed_x = tb.ref_pydantic("computed_x")
+    expected_x = tb.ref_pydantic("expected_x")
+    assert (
+        min(
+            np.linalg.norm(computed_x - expected_x),
+            np.linalg.norm(-computed_x - expected_x),
+        )
+        < 0.05
     )
