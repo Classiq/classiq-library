950e17b45722e60baaf97095ee2b46032b21bdbb
Dor Harpaz <dor@classiq.io>
2025-09-17 13:58:07 +0300

    Add flags to control execution flow

--- .internal/update_outputs/update_notebooks.sh
+++ .internal/update_outputs/update_notebooks.sh
@@ -3,6 +3,23 @@
 # all this script does is wrap the line:
 # 	find . -type f -name "*.ipynb" | xargs -P3 -I{} jupyter nbconvert --to notebook --execute --inplace {}
 
+if [[ " $* " == *" --help "* ]]; then
+  echo "usage: $0"
+  echo "  plus, there are some environment variables which you can set"
+  echo "    UPDATER_SILENCE_PIP - can be set to '1' or 'true'"
+  echo "    UPDATER_MAX_THREADS - any number (recommending 1, 2, or 3)"
+  echo "    UPDATER_MODE        - can be set to 'test_small', 'test_algorithms', 'test_all'"
+  echo "    UPDATER_FOLDER      - can be set to a folder, on which we run 'find <that folder>."
+  echo
+  echo "example usage:"
+  echo "UPDATER_MAX_THREADS=1 UPDATER_SILENCE_PIP=1 UPDATER_FOLDER=algorithms/algebraic .internal/update_outputs/update_notebooks.sh"
+  echo
+  echo "UPDATER_MAX_THREADS=1 UPDATER_SILENCE_PIP=1 UPDATER_MODE=test_small .internal/update_outputs/update_notebooks.sh"
+  exit 0
+fi
+
+
+
 
 #
 # 1) Initialization - `git pull` + create-new-branch
@@ -35,16 +52,34 @@ echo "======= Updating notebooks ======="
 
 current_folder="$(realpath "$(dirname "$0")")"
 
+# Set default values
+MAX_THREADS=${UPDATER_MAX_THREADS:-3}  # note: there's a max of 3 running jobs per user, so we limit the threads to 3
+MODE=${UPDATER_MODE:-}
+FOLDER=${UPDATER_FOLDER:-}
 
-# note: there's a max of 3 running jobs per user, so we limit the threads to 3
-# # test all bernstein-vazirani notebooks
-find . -type f -name "*bernstein*.ipynb" | xargs -P2 -I{} "$current_folder/update_single_notebook_links.sh" {}
-# test all algorithms
-# find algorithms/ -type f -name "*.ipynb" | xargs -P3 -I{} "$current_folder/update_single_notebook_links.sh" {}
-# # test 3 notebooks
-# find . -type f -name "*.ipynb" | head -n 3 | xargs -P3 -I{} "$current_folder/update_single_notebook_links.sh" {}
-# # test all notebooks
-# find algorithms applications tutorials -type f -name "*.ipynb" | xargs -P3 -I{} "$current_folder/update_single_notebook_links.sh" {}
+# Determine the list of files to process
+if [[ -n "$FOLDER" ]]; then
+  FIND_CMD="find \"$FOLDER\" -type f -name \"*.ipynb\""
+else
+  case "$MODE" in
+    test_small)
+      FIND_CMD='find . -type f -name "*bernstein*.ipynb"'
+      ;;
+    test_algorithms)
+      FIND_CMD='find algorithms/ -type f -name "*.ipynb"'
+      ;;
+    test_all)
+      FIND_CMD='find . -type f -name "*.ipynb"'
+      ;;
+    *)
+      echo "Error: Either UPDATER_FOLDER must be set or UPDATER_MODE must be one of: test_small, test_algorithms, test_all."
+      exit 1
+      ;;
+  esac
+fi
+
+# Execute the command
+eval "$FIND_CMD" | xargs -P"$MAX_THREADS" -I{} "$current_folder/update_single_notebook_links.sh" "{}"
 
 #
 # 3) Commit the changes + open PR
