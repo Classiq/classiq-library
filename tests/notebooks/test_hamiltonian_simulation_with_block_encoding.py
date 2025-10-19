9ba5dacb2d40f31450af8bb6915b00e97605d1ab
Tomer Goldfriend <tomer@classiq.io>
2025-10-19 14:31:55 +0300

    add tests

--- tests/notebooks/test_hamiltonian_simulation_with_block_encoding.py
+++ tests/notebooks/test_hamiltonian_simulation_with_block_encoding.py
@@ -1,6 +1,5 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -8,42 +7,50 @@ from testbook.client import TestbookNotebookClient
 import numpy as np
 
 
-@wrap_testbook("hamiltonian_simulation_with_block_encoding", timeout_seconds=600)
+@wrap_testbook("hamiltonian_simulation_with_block_encoding", timeout_seconds=900)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog_1"),
-        expected_width=5,  # actual width: 4
-        expected_depth=100,  # actual depth: 70
+        tb.ref_pydantic("qprog_verify_be"),
+        expected_width=4,  # actual width: 4
+        expected_depth=100,  # actual depth: 60
     )
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog_2"),
-        expected_width=12,  # actual width: 11
-        expected_depth=14000,  # actual depth: 12929
+        tb.ref_pydantic("qprog_gqsp"),
+        expected_width=7,  # actual width: 7
+        expected_depth=32000,  # actual depth: 17709
     )
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog_3"),
-        expected_width=10,  # actual width: 8
-        expected_depth=6500,  # actual depth: 6170
+        tb.ref_pydantic("qprog_qsvt"),
+        expected_width=8,  # actual width: 8
+        expected_depth=6500,  # actual depth: 3435
+    )
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_cheb_lcu"),
+        expected_width=12,  # actual width: 12
+        expected_depth=20000,  # actual depth: 12757
     )
 
     # test notebook content
-    assert np.allclose(
-        np.real(tb.ref_numpy("state_result_1")), tb.ref_numpy("expected_state_1")
-    )
 
-    state_result_2 = tb.ref_numpy("state_result_2")
-    expected_state_2 = tb.ref_numpy("expected_state_2")
-    overlap_2 = tb.ref("overlap_qubitization")
-    assert np.linalg.norm(state_result_2 - expected_state_2) < tb.ref("EPS")
-    assert np.isclose(
-        overlap_2, 0.999, atol=0.01
-    )  # 0.9996884609316635  # should be atol=0.001
+    renormalized_verify_be = tb.ref_numpy("state_result_verify_be")
+    expected_state_be = tb.ref_numpy("expected_state_be")
+    assert np.linalg.norm(renormalized_verify_be - expected_state_be) < 1e-4
 
-    state_result_3 = tb.ref_numpy("state_result_3")
-    expected_state_3 = tb.ref_numpy("expected_state_3")
-    overlap_3 = tb.ref("overlap_qsvt")
-    assert np.linalg.norm(state_result_3 - expected_state_3) < tb.ref("EPS")
-    assert np.isclose(
-        overlap_3, 0.999999, atol=0.01
-    )  # 0.9999998243682983  # should be atol=0.000001  # in another run it was 1.0018339139233956
+    expected_state = tb.ref_numpy("expected_state")
+    EPS = tb.ref_numpy("eps")
+    renormalized_state_gqsp = tb.ref_numpy("renormalized_state_gqsp")
+    renormalized_state_qsvt = tb.ref_numpy("renormalized_state_qsvt")
+    renormalized_state_cheb_lcu = tb.ref_numpy("renormalized_state_cheb_lcu")
+
+    renormalized_states = [
+        renormalized_state_gqsp,
+        renormalized_state_qsvt,
+        renormalized_state_cheb_lcu,
+    ]
+    for state in renormalized_states:
+        overlap = np.abs(
+            np.vdot(renormalized_state_cheb_lcu, expected_state)
+            / np.linalg.norm(renormalized_state_cheb_lcu)
+        )
+        assert np.abs(1 - overlap) < EPS
