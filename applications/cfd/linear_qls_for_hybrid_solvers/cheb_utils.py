958cffd88c88403dad4d75415539438296e900b2
Tomer Goldfriend <tomer@classiq.io>
2025-11-03 00:08:21 +0200

    updated chebyshev utils and a notebook for analysis

--- applications/cfd/linear_qls_for_hybrid_solvers/cheb_utils.py
+++ applications/cfd/linear_qls_for_hybrid_solvers/cheb_utils.py
@@ -7,6 +7,7 @@ from numpy.polynomial.chebyshev import Chebyshev
 
 
 def optimize_inversion_polynomial(w_min, w_max, degree, scale):
+    # Calls Classiq built-in Chebyshev optimization
 
     def target_function(x):
         return scale * (w_min) / x
@@ -24,6 +25,7 @@ def optimize_inversion_polynomial(w_min, w_max, degree, scale):
 
 
 def reciprocal_approx(x, w_min, B, scale):
+    # A smooth approximation for w_min/x
     x = np.asarray(x, dtype=float)
     num = 1.0 - (1.0 - x * x) ** B
     out = np.empty_like(x)
@@ -35,16 +37,11 @@ def reciprocal_approx(x, w_min, B, scale):
 
 def get_numpy_cheb_interpolate(w_min, B, scale, degree):
 
-    # poly = Chebyshev.interpolate(
-    #         lambda x: reciprocal_approx(x, w_min, B, scale), degree, domain=[-1, 1]
-    #     )
-    M = 8 * (degree + 1)
-    k = np.arange(M)
-    xs = np.cos(np.pi * (k + 0.5) / M)  # Chebyshev points (dense)
-    poly = Chebyshev.fit(
-        xs, reciprocal_approx(xs, w_min, B, scale), deg=degree, domain=[-1, 1]
+    # Numpy interpolation
+    degree = (degree // 2) * 2
+    poly = Chebyshev.interpolate(
+        lambda x: reciprocal_approx(x, w_min, B, scale), degree, domain=[-1, 1]
     )
-
     even_coef = poly.coef[::2]
     assert np.allclose(even_coef, 0), "Non odd polynomial"
     poly.coef[::2] = 0
@@ -52,7 +49,7 @@ def get_numpy_cheb_interpolate(w_min, B, scale, degree):
 
 
 def get_numpy_cheb_trimmed(w_min, B, scale, degree, full_degree):
-
+    # Numpy interpolation with full degree, then cutting off to requested degree
     poly = Chebyshev.interpolate(
         lambda x: reciprocal_approx(x, w_min, B, scale), full_degree, domain=[-1, 1]
     )
@@ -64,32 +61,32 @@ def get_numpy_cheb_trimmed(w_min, B, scale, degree, full_degree):
 
 
 def get_cheb_coeff(
-    w_min, degree, w_max=1, scale=1, method="interpolated", epsilon=0.01
+    w_min, degree, w_max=1, scale=1, method="interpolated_in_range", epsilon=0.01
 ):
 
     kappa = 1 / w_min
     B = int(kappa**2 * np.log(kappa / epsilon))
     j0 = int(np.sqrt(B * np.log(4 * B / epsilon)))
+    theoretical_degree = 2 * j0 + 1
     print(
-        f"For error {epsilon}, and given kappa, the needed polynomial degree is: 2*{j0}+1"
+        f"For error {epsilon}, and given kappa, the needed polynomial degree is: {theoretical_degree}"
     )
-    print(f"The Chebyshev LCU requires {j0.bit_length()} qubits")
 
-    if method == "optimized":
+    if method == "interpolated_in_range":
         print(
             f"Performing convex optimization for the Chebyshev interpolation, with degree {degree}"
         )
         return optimize_inversion_polynomial(w_min, w_max, degree, scale)
 
-    if method == "interpolated":
+    if method == "numpy_interpolated":
         print(f"Performing numpy Chebyshev interpolation, with degree {degree}")
         return get_numpy_cheb_interpolate(w_min, B, scale, degree)
 
-    if method == "trimmed":
+    if method == "numpy_trimmed":
         print(
-            f"Performing numpy Chebyshev interpolation, with degree {2*j0+1} and trimming to degree {degree}"
+            f"Performing numpy Chebyshev interpolation, with degree {theoretical_degree} and trimming to degree {degree}"
         )
-        return get_numpy_cheb_trimmed(w_min, B, scale, degree, 2 * j0 + 1)
+        return get_numpy_cheb_trimmed(w_min, B, scale, degree, theoretical_degree)
 
 
 def plot_cheb_inv_approx(pcoefs, w_min, w_max=1, scale=1, half_space=True):
@@ -109,20 +106,39 @@ def plot_cheb_inv_approx(pcoefs, w_min, w_max=1, scale=1, half_space=True):
     target_vals = scale * (w_min) / xj_obj
 
     # Plot the results
-    fig, axes = plt.subplots(len(pcoefs), 1, figsize=(12, 12))
+    fig, axes = plt.subplots(1, 1, figsize=(12, 8))
     axes = np.atleast_1d(axes)
     axes = axes.flatten()
-    for i, (label, coeffs) in enumerate(pcoefs.items()):
-        axes[i].plot(xj_obj, target_vals, "-k", label="Target Function", linewidth=2)
-        axes[i].plot(
+    axes[0].plot(xj_obj, target_vals, "-k", label="Target Function", linewidth=2)
+    for label, coeffs in pcoefs.items():
+        axes[0].plot(
             x_vals,
             eval_odd_cheb_poly(coeffs[1::2], x_vals),
             label=label,
             linestyle="--",
         )
 
-        axes[i].set_xlabel("x")
-        axes[i].set_ylabel(rf"{scale}$\lambda_{{\min}}/x$")
-        axes[i].set_title("Target Function vs. Approximated Polynomial")
-        axes[i].legend()
-        axes[i].grid(True)
+    axes[0].set_xlabel("x")
+    axes[0].set_ylabel(rf"{scale}$\lambda_{{\min}}/x$")
+    axes[0].set_title("Target Function vs. Approximated Polynomial")
+    axes[0].legend()
+    axes[0].grid(True)
+
+    # # Plot the results
+    # fig, axes = plt.subplots(len(pcoefs), 1, figsize=(12, 12))
+    # axes = np.atleast_1d(axes)
+    # axes = axes.flatten()
+    # for i, (label, coeffs) in enumerate(pcoefs.items()):
+    #     axes[i].plot(xj_obj, target_vals, "-k", label="Target Function", linewidth=2)
+    #     axes[i].plot(
+    #         x_vals,
+    #         eval_odd_cheb_poly(coeffs[1::2], x_vals),
+    #         label=label,
+    #         linestyle="--",
+    #     )
+
+    #     axes[i].set_xlabel("x")
+    #     axes[i].set_ylabel(rf"{scale}$\lambda_{{\min}}/x$")
+    #     axes[i].set_title("Target Function vs. Approximated Polynomial")
+    #     axes[i].legend()
+    #     axes[i].grid(True)
