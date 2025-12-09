1fe2be6a947e65a538d182032825f210703bcb78
Tomer Goldfriend <tomer@classiq.io>
2025-12-09 22:46:26 +0200

    add explicit operator transform function

--- applications/cfd/qls_for_hybrid_solvers/classical_functions_be.py
+++ applications/cfd/qls_for_hybrid_solvers/classical_functions_be.py
@@ -1,7 +1,31 @@
 from openfermion import QubitOperator
 import numpy as np
 from scipy.sparse import csr_matrix
-from classiq import SparsePauliOp
+from classiq import SparsePauliOp, SparsePauliTerm, IndexedPauli, Pauli
+
+from typing import cast
+from openfermion.utils.operator_utils import count_qubits
+
+
+# TODO: remove after bug fix
+def of_op_to_cl_op(qubit_op: QubitOperator) -> SparsePauliOp:
+    n_qubits = cast(int, count_qubits(qubit_op))
+    return SparsePauliOp(
+        terms=[
+            SparsePauliTerm(
+                paulis=[  # type:ignore[arg-type]
+                    IndexedPauli(
+                        pauli=getattr(Pauli, pauli),
+                        index=qubit,
+                    )
+                    for qubit, pauli in term
+                ],
+                coefficient=coeff,
+            )
+            for term, coeff in qubit_op.terms.items()
+        ],
+        num_qubits=n_qubits,
+    )
 
 
 def get_projected_state_vector(  # type: ignore[no-untyped-def]
