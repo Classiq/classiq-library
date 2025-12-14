e77c204fc7a02a93556ba205824e1139688129cd
Or Samimi Golan <orsa@classiq.io>
2025-12-14 11:27:28 +0200

    fixed gmqaoa test

--- tests/notebooks/test_gm_qaoa.py
+++ tests/notebooks/test_gm_qaoa.py
@@ -8,11 +8,9 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("gm_qaoa", timeout_seconds=300)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
+        tb.ref_pydantic("qprog_gmqaoa"),
         expected_width=None,
         expected_depth=None,
         expected_cx_count=None,
