dbc5bea5b66932042da6bcc20cdb9ea0d8018079
yigali-classiq <yigal@classiq.io>
2025-12-14 16:20:23 +0200

    Add QAOA in QAOA

--- tests/notebooks/test_qaoa_in_qaoa.py
+++ tests/notebooks/test_qaoa_in_qaoa.py
@@ -6,7 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("qaoa_in_qaoa", timeout_seconds=60)
+@wrap_testbook("qaoa_in_qaoa", timeout_seconds=1801)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
     validate_quantum_model(tb.ref("qmod"))
