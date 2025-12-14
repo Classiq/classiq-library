3ca673a2125963ea00a26fd297aa635d934ece56
ronClassiq <ron@classiq.io>
2025-12-14 15:35:39 +0200

    change width

--- tests/notebooks/test_brownian_chebyshev_polynomials.py
+++ tests/notebooks/test_brownian_chebyshev_polynomials.py
@@ -13,7 +13,7 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
-        expected_width=113,
+        expected_width=135,
         expected_depth=None,
         expected_cx_count=None,
     )
