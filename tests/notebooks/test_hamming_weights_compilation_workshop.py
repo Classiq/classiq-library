3ece1a1578fd178a69d251d36b8347699966fffc
barak <barak@classiq.io>
2025-12-14 17:23:46 +0200

    test file

--- tests/notebooks/test_hamming_weights_compilation_workshop.py
+++ tests/notebooks/test_hamming_weights_compilation_workshop.py
@@ -6,17 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("hamming_weights_compilation_workshop", timeout_seconds=60)
+@wrap_testbook("hamming_weights_compilation_workshop", timeout_seconds=1801)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
-    # test quantum programs
-    validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
-        expected_cx_count=None,
-    )
-
     # test notebook content
     pass  # Todo
