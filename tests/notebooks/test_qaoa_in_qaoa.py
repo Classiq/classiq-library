40fcc9f954b778669643df4bb0a7eebc6fb1ab75
yigali-classiq <yigal@classiq.io>
2025-12-14 17:19:16 +0200

    Add QAOA in QAOA

--- tests/notebooks/test_qaoa_in_qaoa.py
+++ tests/notebooks/test_qaoa_in_qaoa.py
@@ -8,15 +8,6 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("qaoa_in_qaoa", timeout_seconds=1801)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
-    # test quantum programs
-    validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
-        expected_cx_count=None,
-    )
 
     # test notebook content
     pass  # Todo
