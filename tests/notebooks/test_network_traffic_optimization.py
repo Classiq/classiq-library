e7aaf20015723c3a8d612b1f9d3112bb1da431d9
roie-d-classiq <roie.dann@classiq.io>
2025-12-14 12:42:30 +0200

    added a test file

--- tests/notebooks/test_network_traffic_optimization.py
+++ tests/notebooks/test_network_traffic_optimization.py
@@ -8,8 +8,6 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("network_traffic_optimization", timeout_seconds=1800)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
