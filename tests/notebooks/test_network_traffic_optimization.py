e835475fc86a613f2c4a53111c82672320194ba4
roie-d-classiq <roie.dann@classiq.io>
2025-12-14 12:30:16 +0200

    added a test file

--- tests/notebooks/test_network_traffic_optimization.py
+++ tests/notebooks/test_network_traffic_optimization.py
@@ -14,7 +14,7 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
         expected_width=30,  # actual width: 24
-        expected_depth=150,  # actual depth: 126
+        expected_depth=300,  # actual depth: 126
     )
 
     # test notebook content
