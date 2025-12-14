f0d3f99d9cbc0384a5812fcc1014c5596e3f5098
Amir Gruber <amirg@classiq.io>
2025-12-14 12:41:18 +0200

    after test fix

--- tests/notebooks/test_HHL_portfolio.py
+++ tests/notebooks/test_HHL_portfolio.py
@@ -9,7 +9,6 @@ from testbook.client import TestbookNotebookClient
 @wrap_testbook("HHL_portfolio", timeout_seconds=1801)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
-    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
