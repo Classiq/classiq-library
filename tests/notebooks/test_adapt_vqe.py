74e67774eeb588e2a80aba4b34fcef7d4de5a91a
roie-d-classiq <roie.dann@classiq.io>
2025-12-24 12:45:15 +0200

    modified adapt vqe

--- tests/notebooks/test_adapt_vqe.py
+++ tests/notebooks/test_adapt_vqe.py
@@ -6,7 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("adapt_vqe", timeout_seconds=1801)
+@wrap_testbook("adapt_vqe", timeout_seconds=600)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
     validate_quantum_program_size(
