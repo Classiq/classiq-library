8a3491b5e97b84456ea506503ab67bf58827b77c
Or Samimi Golan <orsa@classiq.io>
2026-01-06 18:53:30 +0200

    fixed mcx tutorial

--- tests/notebooks/test_mcx.py
+++ tests/notebooks/test_mcx.py
@@ -29,8 +29,8 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     )
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_4"),
-        expected_width=80,  # actual width: 69
-        expected_depth=1700,  # actual depth: 494 ; also was 920; also was 1550
+        expected_width=90,  # actual width: 75
+        expected_depth=200,  # actual depth: 80
     )
 
     # test notebook content
