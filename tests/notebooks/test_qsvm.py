2d8eef8eafb7ab67eb80ad40f4cc072e1f34bcfa
roie-d-classiq <roie.dann@classiq.io>
2026-01-11 10:12:40 +0200

    updated qsvm test

--- tests/notebooks/test_qsvm.py
+++ tests/notebooks/test_qsvm.py
@@ -6,20 +6,23 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("qsvm", timeout_seconds=204)
+@wrap_testbook("qsvm", timeout_seconds=270)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     """
     A notebook for a hybrid classical quantum neural network.
     The test verifies that the pre-trained model is indeed well trained.
     """
-    # test models
-    validate_quantum_model(tb.ref("QSVM_BLOCH_SHPERE_qmod"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
         expected_width=1,  # actual width: 1
         expected_depth=10,  # actual depth: 4
     )
-
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=4,  # actual width: 2
+        expected_depth=50,  # actual depth: 30
+    )
     # test notebook content
-    assert tb.ref("test_score") == 1
+    assert tb.ref("test_score") >= 0.98
+    assert tb.ref("test_score_pauli") >= 0.98
