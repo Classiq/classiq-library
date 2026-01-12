ce1a9f439990f074691ad1a573493187554ca433
Tomer Goldfriend <tomer@classiq.io>
2026-01-13 00:27:33 +0200

    fix hamiltonian simulation with be test and nb, due to change of inplace xor implementation

--- tests/notebooks/test_hamiltonian_simulation_with_block_encoding.py
+++ tests/notebooks/test_hamiltonian_simulation_with_block_encoding.py
@@ -22,8 +22,8 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     )
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_qsvt"),
-        expected_width=8,  # actual width: 8
-        expected_depth=6500,  # actual depth: 2458
+        expected_width=7,  # actual width: 7
+        expected_depth=7500,  # actual depth: 6090
     )
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_cheb_lcu"),
