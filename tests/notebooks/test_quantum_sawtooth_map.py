c2c06d8c86e222f138a3887e8be62c867fa3329b
Tomer Goldfriend <tomer@classiq.io>
2026-02-09 09:20:00 +0200

    with test and assertion

--- tests/notebooks/test_quantum_sawtooth_map.py
+++ tests/notebooks/test_quantum_sawtooth_map.py
@@ -1,6 +1,5 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -8,15 +7,16 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("quantum_sawtooth_map", timeout_seconds=60)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
-        expected_cx_count=None,
+        expected_width=5,  # actual 3
+        expected_depth=100,  # actual 49
     )
 
     # test notebook content
-    pass  # Todo
+    timesteps = tb.ref("timesteps")
+    res_simulator = tb.ref_pydantic("results_simulator")
+    for i in range(len(timesteps)):
+        df = res_simulator[i].dataframe
+        assert float(df.loc[df["p"] == -2, "probability"].iloc[0]) > 0.9
