e08def636d25e1391d960ef9c8aa3718b4c2266f
Dor Harpaz <dor@classiq.io>
2026-02-12 11:42:46 +0200

    Remove timeouts internal file

--- .internal/pre_commit_tools/notebook_pre_commit_collection.py
+++ .internal/pre_commit_tools/notebook_pre_commit_collection.py
@@ -11,11 +11,6 @@ from pathlib import Path
 import yaml
 
 PROJECT_ROOT = Path(subprocess.getoutput("git rev-parse --show-toplevel"))  # noqa: S605
-TIMEOUTS_FILE = PROJECT_ROOT / "tests" / "resources" / "timeouts.yaml"
-
-DEFAULT_TIMEOUT_SECONDS: float = 20
-
-ENFORCE_TIMEOUTS: bool = False
 
 
 def main(full_file_paths: Iterable[str]) -> bool:
@@ -41,27 +36,6 @@ def is_valid_notebook(file_path: str, automatically_add_timeout: bool = True) ->
             f"    for example, you may change '{file_path}' to '{file_path.replace(' ', '_')}'."
         )
 
-    if (
-        ENFORCE_TIMEOUTS
-        and not _is_file_in_timeouts(file_name)
-        and should_notebook_be_tested(file_path)
-    ):
-        if automatically_add_timeout:
-            _add_file_to_timeouts(file_name)
-            errors.append(
-                "A new notebook was detected.\n"
-                f"    Automatically adding a timeout entry {{{file_name} : {DEFAULT_TIMEOUT_SECONDS}}}.\n"
-                f"    Please make sure to add the changes done to '{TIMEOUTS_FILE}'"
-            )
-        else:
-            errors.append(
-                "A new notebook was detected.\n"
-                "    However, a coresponding entry in the timeouts file is missing.\n"
-                f"    Please add an entry. You may add '{{{file_name} : {DEFAULT_TIMEOUT_SECONDS}}}'\n"
-                f"        to {TIMEOUTS_FILE}\n"
-                "    Alternatively, you may install pre-commit. It will automatically add a timeout entry in the next time you run 'git commit'."
-            )
-
     if errors:
         spacing = "\n\t"  # f-string cannot include backslash
         print(f"File `{file_path}` has error(s):{spacing}{spacing.join(errors)}")
@@ -79,23 +53,6 @@ def _does_contain_dash_in_file_name(file_name: str) -> bool:
 
 def _does_contain_space_in_file_name(file_name: str) -> bool:
     return " " in file_name
-
-
-def _is_file_in_timeouts(file_name: str) -> bool:
-    with TIMEOUTS_FILE.open("r") as f:
-        timeouts = yaml.safe_load(f)
-
-    return file_name in timeouts
-
-
-def _add_file_to_timeouts(file_name: str) -> None:
-    with TIMEOUTS_FILE.open("r") as f:
-        timeouts = yaml.safe_load(f)
-
-    timeouts[file_name] = DEFAULT_TIMEOUT_SECONDS
-
-    with TIMEOUTS_FILE.open("w") as f:
-        yaml.dump(timeouts, f, sort_keys=True)
 
 
 def validate_unique_names() -> bool:
