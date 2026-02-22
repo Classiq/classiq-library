05029b92d5745e9c04fa00ab6a8e78f0139b705b
Tomer Goldfriend <tomer@classiq.io>
2026-02-23 01:23:37 +0200

    set dummy test file

--- tests/notebooks/test_resiliency_planning_AMD.py
+++ tests/notebooks/test_resiliency_planning_AMD.py
@@ -1,10 +1,3 @@
-from tests.utils_for_testbook import (
-    wrap_testbook,
-)
-from testbook.client import TestbookNotebookClient
-
-
-@wrap_testbook("resiliency_planning_AMD", timeout_seconds=60)
-def test_notebook(tb: TestbookNotebookClient) -> None:
+def test_notebook() -> None:
     # test notebook content
     pass  # Todo
