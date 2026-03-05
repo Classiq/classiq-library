a36a4173d3d67aa29a81a87c0121336e1a716ab0
king-p3nguin <kazukitsuoka@g.ecc.u-tokyo.ac.jp>
2026-03-06 03:57:34 +0900

    change qdo notebook and test (fix ongoing)

--- tests/notebooks/test_quantum_drude_oscillator.py
+++ tests/notebooks/test_quantum_drude_oscillator.py
@@ -7,16 +7,15 @@ from tests.utils_for_testbook import validate_quantum_program_size, wrap_testboo
 @wrap_testbook("quantum_drude_oscillator", timeout_seconds=500)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
-    for qprog in tb.ref_pydantic("qprogs"):
-        validate_quantum_program_size(
-            qprog,
-            expected_width=4,  # actual width: 4
-            expected_depth=11,  # actual depth: 11
-        )
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=4,  # actual width: 4
+        expected_depth=11,  # actual depth: 11
+    )
 
     # test notebook content
     for duration in tb.ref("durations"):
-        assert duration < 50  # locally it is no longer than 50 seconds
+        assert duration < 15  # locally it is no longer than 15 seconds
 
     assert np.isclose(
         tb.ref("VQE_energy")[-1], tb.ref("exact_energies")[-1], atol=0.01
