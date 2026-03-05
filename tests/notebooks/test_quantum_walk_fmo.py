5138d6a5fd6282d031d9ae46e170bdea1cf6160f
king-p3nguin <kazukitsuoka@g.ecc.u-tokyo.ac.jp>
2026-03-05 17:13:04 +0900

    update qw_fmo notebook and test

--- tests/notebooks/test_quantum_walk_fmo.py
+++ tests/notebooks/test_quantum_walk_fmo.py
@@ -4,19 +4,19 @@ from testbook.client import TestbookNotebookClient
 from tests.utils_for_testbook import validate_quantum_program_size, wrap_testbook
 
 
-@wrap_testbook("quantum_walk_fmo", timeout_seconds=150)
+@wrap_testbook("quantum_walk_fmo", timeout_seconds=100)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test quantum programs
-    for qprog in tb.ref_pydantic("qprogs"):
-        validate_quantum_program_size(
-            qprog,
-            expected_width=3,  # actual width: 3
-            expected_depth=800,  # actual max depth: 761
-        )
+    # test quantum program
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=3,  # actual width: 3
+        expected_depth=None,
+        expected_cx_count=None,
+    )
 
     # test notebook content
     for duration in tb.ref("durations"):
-        assert duration < 15  # locally it is no longer than 15 seconds
+        assert duration < 5  # locally it is no longer than 5 seconds
 
     assert np.isclose(
         tb.ref("trotter_results")[0][0],
