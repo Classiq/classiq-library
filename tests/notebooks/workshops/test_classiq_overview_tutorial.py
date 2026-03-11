4019e9db297892c019faeb8e9021f533c0bece43
Alexandre Ricardo <alexandre@classiq.io>
2026-03-11 11:04:31 -0300

    Updating tests

--- tests/notebooks/workshops/test_classiq_overview_tutorial.py
+++ tests/notebooks/workshops/test_classiq_overview_tutorial.py
@@ -8,8 +8,6 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("classiq_overview_tutorial", timeout_seconds=500)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
 
     # test quantum programs
     validate_quantum_program_size(
