aab3a92df6b1e759cd241aefbfe87e1722e1afcf
TamuzDanzig1 <118278794+TamuzDanzig1@users.noreply.github.com>
2026-03-14 16:38:04 +0200

    rainbow workshop, json and test

---
+++ tests/notebooks/workshops/test_rainbow_options_workshop_bruteforce.py
@@ -0,0 +1,19 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("rainbow_options_workshop_bruteforce", timeout_seconds=200)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test quantum programs state preparation of the iqae
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=23,
+    )
+    # test quantum program of full the iqae
+    validate_quantum_program_size(
+        tb.ref_pydantic("iqae_qprog"),
+        expected_width=25,
+    )
