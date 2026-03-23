401cedbb546c5621c03ce49019e8dd358f4c9638
roie-d-classiq <roie.dann@classiq.io>
2026-03-23 17:07:46 +0200

    updated timeout in test

--- tests/notebooks/test_fermi_hubbard_1D.py
+++ tests/notebooks/test_fermi_hubbard_1D.py
@@ -6,7 +6,7 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("fermi_hubbard_1D", timeout_seconds=60)
+@wrap_testbook("fermi_hubbard_1D", timeout_seconds=1801)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test models
     validate_quantum_model(tb.ref("qmod"))
