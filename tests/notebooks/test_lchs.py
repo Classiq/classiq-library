4340f9fd21aa83023a9c04c5fb967528a0ad9958
Or Samimi Golan <orsa@classiq.io>
2026-04-05 12:37:03 +0300

    added test file

---
+++ tests/notebooks/test_lchs.py
@@ -0,0 +1,19 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("lchs", timeout_seconds=180)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test quantum programs
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog"),
+        expected_width=None,
+        expected_depth=None,
+        expected_cx_count=None,
+    )
+
+    # test notebook content
+    pass  # Todo
