a6eb7f35d86a99021605ac291577f37aad385057
Tomer Goldfriend <tomer@classiq.io>
2026-05-13 00:25:04 +0300

    update chebyshev approximation notebook

--- tests/notebooks/test_chebyshev_approximation.py
+++ tests/notebooks/test_chebyshev_approximation.py
@@ -6,5 +6,12 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("chebyshev_approximation", timeout_seconds=60)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test notebook content
-    pass  # Todo
+
+    actual_depths = [1180, 56]
+    qprogs = tb.ref_pydantic("qprogs")
+    for act_d, qp in zip(actual_depths, qprogs):
+        validate_quantum_program_size(
+            qp,
+            expected_width=7,  # actual width: 7
+            expected_depth=2 * act_d,
+        )
