694ca45f2c1ba64896243181538dfc21bffb27a4
Ron Hass <ronhass@classiq.io>
2026-05-12 10:41:00 +0300

    Update arithmetic expressions notebook

--- tests/notebooks/test_arithmetic_expressions.py
+++ tests/notebooks/test_arithmetic_expressions.py
@@ -1,6 +1,5 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -8,20 +7,16 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("arithmetic_expressions", timeout_seconds=40)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
-    validate_quantum_model(tb.ref("qmod_1"))
-    validate_quantum_model(tb.ref("qmod_2"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_1"),
         expected_width=tb.ref("NUM_QUBITS_1"),
-        expected_depth=380,  # actual depth: 321
+        expected_depth=300,  # actual depth: 222
     )
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_2"),
         expected_width=tb.ref("NUM_QUBITS_2"),
-        expected_depth=270,  # actual depth: 228
+        expected_depth=270,  # actual depth: 200
     )
 
     # test notebook content
