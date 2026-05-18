be12a71bef09faa291a9e7f0306b655962b4ee07
Tomer Goldfriend <tomer@classiq.io>
2026-05-18 15:14:37 +0300

    fix test

--- tests/notebooks/test_hamiltonian_simulation_guide.py
+++ tests/notebooks/test_hamiltonian_simulation_guide.py
@@ -38,24 +38,19 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
         expected_width=2,  # actual width: 2
         expected_depth=600,  # actual depth: 480
     )
-    # widths:
-    #   [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
-    # depths:
-    #   [0,11,11,11,11,11,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20]
-    for qprog in tb.ref_pydantic("qprogs_magnetization_exponentiation")[1:]:
-        validate_quantum_program_size(
-            qprog,
-            expected_width=2,  # actual width: 2
-            expected_depth=20,  # actual depth: 20
-        )
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_magnetization_exponentiate"),
+        expected_width=2,  # actual width: 2
+        expected_depth=20,  # actual depth: 20
+    )
 
     # test notebook content
     time_list = tb.ref("time_list")
     results_ST = tb.ref_numpy("magnetization_ST")
-    results_ewdc = tb.ref_numpy("magnetization_ewdc")
+    results_exponentiate = tb.ref_numpy("magnetization_exponentiate")
     results_qdrift = tb.ref_numpy("magnetization_qdrift")
 
     tolerance = 0.1  # that's a large tolerance. we should lower it.
-    np.allclose(results_ST, results_ewdc, atol=tolerance)
+    np.allclose(results_ST, results_exponentiate, atol=tolerance)
     np.allclose(results_ST, results_qdrift, atol=tolerance)
-    np.allclose(results_ewdc, results_qdrift, atol=tolerance)
+    np.allclose(results_exponentiate, results_qdrift, atol=tolerance)
