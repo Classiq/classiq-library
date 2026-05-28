99313c63b0de2705dd2209e2c04cf59c0fb88ee9
Tomer Goldfriend <tomer@classiq.io>
2026-05-28 14:49:28 +0300

    fix sp_approx notebook for structure data and test, fix qaoa examples as well

--- tests/notebooks/test_approximated_state_preparation.py
+++ tests/notebooks/test_approximated_state_preparation.py
@@ -10,31 +10,22 @@ from testbook.client import TestbookNotebookClient
 )  # took 1860 seconds on my computer
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
-    # classiq depths: [114, 8662, 7853, 7622, 7392, 6932, 6702, 6472, 6241, 6010]
-    # cx-counts depths: [62, 5722, 5171, 5011, 4851, 4531, 4371, 4211, 4051, 3891]
-    # The depth has to improve from the previous runs. The difference between runs 0 and 1 is an exception,
-    # since for 0 error we use a different algorithm for exact state preparation.
+    # The depth has to improve from the previous runs.
     qprogs = tb.ref_pydantic("qprogs")
     depths = tb.ref("depths")
+    cx_counts = tb.ref("cx_counts")
     validate_quantum_program_size(
         qprogs[0],
         expected_width=tb.ref("NUM_QUBITS"),
-        expected_depth=300,  # actual depth: 114
+        expected_depth=600,  # actual depth: 493
+        expected_cx_count=350,  # actual cx: 254
     )
-    validate_quantum_program_size(
-        qprogs[1],
-        expected_width=tb.ref("NUM_QUBITS"),
-        expected_depth=10000,  # actual depth: 8662
-    )
-    for i in range(2, len(qprogs)):
+    for i in range(1, len(qprogs)):
         validate_quantum_program_size(
             qprogs[i],
             expected_width=tb.ref("NUM_QUBITS"),
-            expected_depth=depths[i - 1]
-            - 1,  # the depth has to improve from the previous runs
+            expected_depth=depths[
+                i - 1
+            ],  # the depth has to improve from the previous runs
+            expected_cx_count=cx_counts[i - 1],
         )
-
-    # test notebook content
-    cx_counts = tb.ref("cx_counts")
-    for i in range(2, len(cx_counts)):
-        assert cx_counts[i] < cx_counts[i - 1]
--- tests/notebooks/test_qaoa.py
+++ tests/notebooks/test_qaoa.py
@@ -16,7 +16,7 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     )
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_knapsack"),
-        expected_width=18,  # actual width: 18
+        expected_width=21,  # actual width: 21
         expected_depth=2500,  # actual depth: 2323
     )
 
