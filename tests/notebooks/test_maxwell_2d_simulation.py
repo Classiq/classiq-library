3cf88ac18624ef6353235d8f1b8add446f0c3703
roie-d-classiq <roie.dann@classiq.io>
2026-05-28 14:02:35 +0300

    modified the test

--- tests/notebooks/test_maxwell_2d_simulation.py
+++ tests/notebooks/test_maxwell_2d_simulation.py
@@ -5,14 +5,13 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("maxwell_2d_simulation", timeout_seconds=60)
+@wrap_testbook("maxwell_2d_simulation", timeout_seconds=1400)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
-        expected_width=None,
+        expected_width=25,  # actual width: 20
         expected_depth=None,
-        expected_cx_count=None,
     )
 
     # test notebook content
