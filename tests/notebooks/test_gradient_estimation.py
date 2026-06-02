ade0c391637f79ed34934c41ccc0627345c74669
Ofri Wienner <ofriwi1@gmail.com>
2026-06-02 13:18:44 +0300

    Final version

--- tests/notebooks/test_gradient_estimation.py
+++ tests/notebooks/test_gradient_estimation.py
@@ -5,14 +5,49 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("gradient_estimation", timeout_seconds=60)
+@wrap_testbook("gradient_estimation", timeout_seconds=200)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
     validate_quantum_program_size(
-        tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
-        expected_cx_count=None,
+        tb.ref_pydantic("qprog_ancilla"),
+        expected_width=16,  # Expected 8
+        expected_depth=63,  # Expected 42
+        expected_cx_count=41,  # Expected 27
+    )
+
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_linear"),
+        expected_width=6,  # Expected 3
+        expected_depth=23,  # Expected 15
+        expected_cx_count=18,  # Expected 9
+    )
+
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_linear_2"),
+        expected_width=6,  # Expected 3
+        expected_depth=23,  # Expected 15
+        expected_cx_count=18,  # Expected 9
+    )
+
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_quadratic"),
+        expected_width=6,  # Expected 3
+        expected_depth=35,  # Expected 23
+        expected_cx_count=23,  # Expected 15
+    )
+
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_quadratic_2"),
+        expected_width=6,  # Expected 3
+        expected_depth=36,  # Expected 24
+        expected_cx_count=23,  # Expected 15
+    )
+
+    validate_quantum_program_size(
+        tb.ref_pydantic("qprog_2d"),
+        expected_width=12,  # Expected 6
+        expected_depth=68,  # Expected 45
+        expected_cx_count=72,  # Expected 48
     )
 
     # test notebook content
