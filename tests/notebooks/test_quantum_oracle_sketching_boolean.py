336672d001c5a193c357bb4000fc049db6a9ba64
roie-d-classiq <roie.dann@classiq.io>
2026-06-03 15:32:29 +0300

    adjusted test file

--- tests/notebooks/test_quantum_oracle_sketching_boolean.py
+++ tests/notebooks/test_quantum_oracle_sketching_boolean.py
@@ -5,14 +5,14 @@ from tests.utils_for_testbook import (
 from testbook.client import TestbookNotebookClient
 
 
-@wrap_testbook("quantum_oracle_sketching_boolean", timeout_seconds=60)
+@wrap_testbook("quantum_oracle_sketching_boolean", timeout_seconds=800)
 def test_notebook(tb: TestbookNotebookClient) -> None:
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
-        expected_width=None,
-        expected_depth=None,
-        expected_cx_count=None,
+        expected_width=3,
+        expected_depth=2500,
+        expected_cx_count=1300,
     )
 
     # test notebook content
