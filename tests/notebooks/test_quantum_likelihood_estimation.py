9b7cfa71f367680bbf2aa8ce8ef7b53cb0619f04
Tomer Goldfriend <tomer@classiq.io>
2026-06-11 10:52:57 +0300

    add qle example and test

---
+++ tests/notebooks/test_quantum_likelihood_estimation.py
@@ -0,0 +1,17 @@
+from tests.utils_for_testbook import (
+    validate_quantum_program_size,
+    wrap_testbook,
+)
+from testbook.client import TestbookNotebookClient
+
+
+@wrap_testbook("quantum_likelihood_estimation", timeout_seconds=300)
+def test_notebook(tb: TestbookNotebookClient) -> None:
+    # test quantum programs
+    qprogs = tb.ref_pydantic("qprogs")
+    for qprog in qprogs:
+        validate_quantum_program_size(
+            qprog,
+            expected_width=1,  # actual 1
+            expected_depth=12,  # actual 6
+        )
