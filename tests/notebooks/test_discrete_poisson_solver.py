f03b20b773d6553e8d8521a3c1f7be6b4d3cc841
Dor Harpaz <dor@classiq.io>
2026-06-25 14:38:15 +0300

    Update tests to remove qmod tests

--- tests/notebooks/test_discrete_poisson_solver.py
+++ tests/notebooks/test_discrete_poisson_solver.py
@@ -8,8 +8,6 @@ from testbook.client import TestbookNotebookClient
 
 @wrap_testbook("discrete_poisson_solver", timeout_seconds=300)
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog"),
