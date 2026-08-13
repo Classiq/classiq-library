a679679df8a63795beae15e68849c018dba1fd9e
Alexandre Ricardo <alexandre@classiq.io>
2026-08-13 15:35:11 -0300

    updating more tests

--- tests/notebooks/test_simon.py
+++ tests/notebooks/test_simon.py
@@ -1,5 +1,6 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
+    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -11,6 +12,11 @@ def test_notebook(tb: TestbookNotebookClient) -> None:
     A notebook for a hybrid classical quantum neural network.
     The test verifies that the pre-trained model is indeed well trained.
     """
+    # test models
+    validate_quantum_model(tb.ref("qmod_1"))
+    validate_quantum_model(tb.ref("qmod_2"))
+    validate_quantum_model(tb.ref("qmod_3"))
+    validate_quantum_model(tb.ref("qmod_4"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_1"),
