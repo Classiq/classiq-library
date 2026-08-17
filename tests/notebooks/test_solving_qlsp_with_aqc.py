503fa320cb19785d2a686d4404994a5439b9a86c
Alexandre Ricardo <alexandre@classiq.io>
2026-08-17 17:01:19 -0300

    Updating according to review

--- tests/notebooks/test_solving_qlsp_with_aqc.py
+++ tests/notebooks/test_solving_qlsp_with_aqc.py
@@ -1,6 +1,5 @@
 from tests.utils_for_testbook import (
     validate_quantum_program_size,
-    validate_quantum_model,
     wrap_testbook,
 )
 from testbook.client import TestbookNotebookClient
@@ -11,10 +10,6 @@ from testbook.client import TestbookNotebookClient
     timeout_seconds=800,
 )
 def test_notebook(tb: TestbookNotebookClient) -> None:
-    # test models
-    validate_quantum_model(tb.ref("qmod_1"))
-    validate_quantum_model(tb.ref("qmod_2"))
-    validate_quantum_model(tb.ref("qmod_3"))
     # test quantum programs
     validate_quantum_program_size(
         tb.ref_pydantic("qprog_1"),
